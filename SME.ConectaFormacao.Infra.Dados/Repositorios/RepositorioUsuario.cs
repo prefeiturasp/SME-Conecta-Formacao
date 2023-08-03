fd26d8f4047e1035cbb3f20c97b67dd915be3055
Juliano Martins <juliano.campos@amcom.com.br>
2023-08-03 16:22:15 -0400

    AB#100290 - Criar tabela de Usuário

---
+++ SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioUsuario.cs
@@ -0,0 +1,13 @@
+﻿using SME.ConectaFormacao.Dominio;
+using SME.ConectaFormacao.Dominio.Contexto;
+using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
+
+namespace SME.ConectaFormacao.Infra.Dados.Repositorios
+{
+    public class RepositorioUsuario : RepositorioBaseAuditavel<Usuario>, IRepositorioUsuario
+    {
+        public RepositorioUsuario(IContextoAplicacao contexto, IConectaFormacaoConexao conexao) : base(contexto, conexao)
+        {
+        }
+    }
+}
