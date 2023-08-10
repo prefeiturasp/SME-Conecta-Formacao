52c58c92b14e1715cf332606959cf34b21bb99d8
Juliano Martins <juliano.campos@amcom.com.br>
2023-08-10 15:55:55 -0400

    Bug 101255: [96871: [Conecta Formação] Login] - Ajuste de textos de label no campo de Login

--- SME.ConectaFormacao.Dominio/Constantes/MensagemNegocio.cs
+++ SME.ConectaFormacao.Dominio/Constantes/MensagemNegocio.cs
@@ -2,9 +2,9 @@ namespace SME.ConectaFormacao.Dominio.Constantes;
 
 public class MensagemNegocio
 {
-    public const string PERFIS_DO_USUARIO_NAO_LOCALIZADOS_VERIFIQUE_O_LOGIN = "Os perfis do usuário não foram localizados! Verifique o login.";
+    public const string PERFIS_DO_USUARIO_NAO_LOCALIZADOS_VERIFIQUE_O_LOGIN = "Os perfis do usuário não foram localizados! Verifique o usuário.";
     public const string USUARIO_OU_SENHA_INVALIDOS = "Usuário ou senha inválidos";
     public const string CONFIRMACAO_SENHA_INVALIDA = "Confirmação de senha não confere";
     public static string SENHA_NAO_ATENDE_CRITERIOS_SEGURANCA = "A senha não atendo os critérios de segurança: \r\nUma letra maiúscula \r\nUma letra minúscula \r\nUm algarismo (número) ou um símbolo (caractere especial) \r\nNão é permitido caracteres acentuados \r\nDeve ter no mínimo 8 e no máximo 12 caracteres.";
-    public const string LOGIN_OU_SENHA_ATUAL_NAO_COMFEREM = "Login ou senha atual não conferem";
+    public const string LOGIN_OU_SENHA_ATUAL_NAO_COMFEREM = "Usuário ou senha atual não conferem";
 }
