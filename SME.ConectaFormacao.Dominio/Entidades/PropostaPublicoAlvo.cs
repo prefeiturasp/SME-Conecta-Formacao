8d5214dca56fc41ab201adfd7ec7895b7f4dd477
Juliano Martins <juliano.campos@amcom.com.br>
2023-08-28 10:55:14 -0400

    Task 101860: Script para criar tabela proposta

---
+++ SME.ConectaFormacao.Dominio/Entidades/PropostaPublicoAlvo.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.ConectaFormacao.Dominio.Entidades
+{
+    public class PropostaPublicoAlvo : EntidadeBaseAuditavel
+    {
+        public long PropostaId { get; set; }
+        public long CargoFuncaoId { get; set; }
+    }
+}
