ab001f6698f2fd945427c3a4876cbac392c60a3d
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-08-29 13:58:43 -0300

    Criando endpoint de modalidades

---
+++ SME.ConectaFormacao.Aplicacao/Consultas/Proposta/ObterModalidade/ObterModalidadeQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Aplicacao.Dtos;
+using SME.ConectaFormacao.Dominio.Enumerados;
+using SME.ConectaFormacao.Dominio.Extensoes;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class ObterModalidadeQueryHandler : IRequestHandler<ObterModalidadeQuery, IEnumerable<RetornoListagemDTO>>
+    {
+        public Task<IEnumerable<RetornoListagemDTO>> Handle(ObterModalidadeQuery request, CancellationToken cancellationToken)
+        {
+            var lista = Enum.GetValues(typeof(Modalidade))
+                .Cast<Modalidade>()
+                .Select(v => new RetornoListagemDTO
+                {
+                    Id = (short)v,
+                    Descricao = v.Nome()
+                });
+
+            return Task.FromResult(lista);
+        }
+    }
+}
