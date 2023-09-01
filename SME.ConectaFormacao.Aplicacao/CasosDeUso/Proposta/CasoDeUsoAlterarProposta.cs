c723735d7e4c0d79c6189595a7dbe9e8fdf528ed
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-01 16:12:42 -0400

    101867 - salvar alteração rascunho

--- SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoAlterarProposta.cs
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoAlterarProposta.cs
@@ -12,6 +12,9 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Proposta
 
         public async Task<long> Executar(long id, PropostaDTO propostaDTO)
         {
+
+            if (propostaDTO.Situacao == Dominio.Enumerados.SituacaoRegistro.Rascunho)
+                return await mediator.Send(new AlterarPropostaRascunhoCommand(id, propostaDTO));
             return await mediator.Send(new AlterarPropostaCommand(id, propostaDTO));
         }
     }
