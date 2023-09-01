3ec65e604d1fe5f34b737551f2505ccd89937553
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-01 10:23:11 -0400

    Task 101870: Criar endpoint para obter proposta por id

---
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoObterPropostaPorId.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Aplicacao.Dtos.Proposta;
+using SME.ConectaFormacao.Aplicacao.Interfaces.Proposta;
+
+namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Proposta
+{
+    public class CasoDeUsoObterPropostaPorId : CasoDeUsoAbstrato, ICasoDeUsoObterPropostaPorId
+    {
+        public CasoDeUsoObterPropostaPorId(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<PropostaCompletoDTO> Executar(long id)
+        {
+            return await mediator.Send(new ObterPropostaPorIdQuery(id));
+        }
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Consultas/Proposta/ObterPropostaPorId/ObterPropostaPorIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.ConectaFormacao.Aplicacao.Dtos.Proposta;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class ObterPropostaPorIdQuery : IRequest<PropostaCompletoDTO>
+    {
+        public ObterPropostaPorIdQuery(long id)
+        {
+            Id = id;
+        }
+
+        public long Id { get; }
+    }
+
+    public class ObterPropostaPorIdQueryValidator : AbstractValidator<ObterPropostaPorIdQuery>
+    {
+        public ObterPropostaPorIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("É nescessário informar o id para obter a proposta");
+        }
+    }
+}
