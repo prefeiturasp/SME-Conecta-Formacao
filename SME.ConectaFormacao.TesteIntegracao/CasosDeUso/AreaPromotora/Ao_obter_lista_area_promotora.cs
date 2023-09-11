d13f09f6da7eec9c1624557ebf47f574be5af5db
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-11 17:14:51 -0400

    Task 101934: Criar endpoint área promotora

---
+++ SME.ConectaFormacao.TesteIntegracao/CasosDeUso/AreaPromotora/Ao_obter_lista_area_promotora.cs
@@ -0,0 +1,40 @@
+﻿using Shouldly;
+using SME.ConectaFormacao.Aplicacao.Interfaces.AreaPromotora;
+using SME.ConectaFormacao.TesteIntegracao.CasosDeUso.AreaPromotora.Mock;
+using SME.ConectaFormacao.TesteIntegracao.Setup;
+using Xunit;
+
+namespace SME.ConectaFormacao.TesteIntegracao.CasosDeUso.AreaPromotora
+{
+    public class Ao_obter_lista_area_promotora : TesteBase
+    {
+        public Ao_obter_lista_area_promotora(CollectionFixture collectionFixture) : base(collectionFixture)
+        {
+        }
+
+        [Fact(DisplayName = "Área promotora - Deve obter lista")]
+        public async Task Deve_obter_lista()
+        {
+            // arrange 
+            var areasPromotoras = AreaPromotoraMock.GerarAreaPromotora(10);
+            await InserirNaBase(areasPromotoras);
+
+            var casoDeUso = ObterCasoDeUso<ICasoDeUsoObterAreaPromotoraLista>();
+
+            // act 
+            var retorno = await casoDeUso.Executar();
+
+            // assert 
+            retorno.Any().ShouldBeTrue();
+            retorno.Count().ShouldBe(areasPromotoras.Count());
+
+            foreach (var item in retorno)
+            {
+                var areaPromotora = areasPromotoras.FirstOrDefault(t => t.Id == item.Id);
+
+                areaPromotora.ShouldNotBeNull();
+                item.Descricao.ShouldBe(areaPromotora.Nome);
+            }
+        }
+    }
+}
