e6741a6b9f585f3d317cd60721b6bccf587506bc
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-19 17:17:09 -0400

    utf-8

--- scripts/V10__ALTERACAO_ROTEIROPROPOSTA_FORMACAO.sql
+++ scripts/V10__ALTERACAO_ROTEIROPROPOSTA_FORMACAO.sql
@@ -1,15 +1,15 @@
 ALTER TABLE roteiro_proposta_formativa ALTER COLUMN descricao TYPE text;
 
-update roteiro_proposta_formativa set descricao = '<p>Este roteiro tem como finalidade auxiliar na elaboraÁ„o das propostas formativas no ‚mbito da Rede Municipal de Ensino.</p> 
-<p>O tema È priorit·rio para o desenvolvimento dos programas e projetos da SME?</p>
-<p>A carga hor·ria apresentada est· em acordo com o disposto no Edital NTF 2023?</p>
-<p>A justificativa apresenta diagnÛstico da realidade local e/ou necessidade de continuidade ou aprofundamento no tema?</p> 
-<p>H· articulaÁ„o entre objetivos, tema, metodologia, conte˙do, forma de abordagem e carga hor·ria?</p> 
-<p>O conte˙do program·tico est· alinhado a um ou mais princÌpios do Edital NTF 2023, com foco na melhoria das aprendizagens dos estudantes?</p> 
-<p>A carga hor·ria possibilita a exploraÁ„o do conte˙do apresentado de forma satisfatÛria, permitindo o aprofundamento na tem·tica?</p> 
-<p>A metodologia e quantidade de participantes por turma s„o adequadas para promover a aquisiÁ„o de saberes pelos cursistas e est„o em acordo com o Edital NTF 2023?</p> 
-<p>A proposta tem como princÌpio metodolÛgico o uso da problematizaÁ„o? Os procedimentos metodolÛgicos favorecem a relaÁ„o entre a teoria e a pr·tica profissional?</p> 
-<p>As referÍncias bibliogr·ficas atendem ao conte˙do program·tico?</p> 
-<p>Os objetivos desta formaÁ„o ser„o atingidos considerando o p˙blico-alvo proposto? O corpo docente tem formaÁ„o ou experiÍncia na tem·tica do curso?</p> 
-<p>No caso das formaÁıes a dist‚ncia, a relaÁ„o entre tutores e cursistas no ambiente virtual de aprendizagem permite a mediaÁ„o satisfatÛria?</p>'
+update roteiro_proposta_formativa set descricao = '<p>Este roteiro tem como finalidade auxiliar na elabora√ß√£o das propostas formativas no √¢mbito da Rede Municipal de Ensino.</p> 
+<p>O tema √© priorit√°rio para o desenvolvimento dos programas e projetos da SME?</p>
+<p>A carga hor√°ria apresentada est√° em acordo com o disposto no Edital NTF 2023?</p>
+<p>A justificativa apresenta diagn√≥stico da realidade local e/ou necessidade de continuidade ou aprofundamento no tema?</p> 
+<p>H√° articula√ß√£o entre objetivos, tema, metodologia, conte√∫do, forma de abordagem e carga hor√°ria?</p> 
+<p>O conte√∫do program√°tico est√° alinhado a um ou mais princ√≠pios do Edital NTF 2023, com foco na melhoria das aprendizagens dos estudantes?</p> 
+<p>A carga hor√°ria possibilita a explora√ß√£o do conte√∫do apresentado de forma satisfat√≥ria, permitindo o aprofundamento na tem√°tica?</p> 
+<p>A metodologia e quantidade de participantes por turma s√£o adequadas para promover a aquisi√ß√£o de saberes pelos cursistas e est√£o em acordo com o Edital NTF 2023?</p> 
+<p>A proposta tem como princ√≠pio metodol√≥gico o uso da problematiza√ß√£o? Os procedimentos metodol√≥gicos favorecem a rela√ß√£o entre a teoria e a pr√°tica profissional?</p> 
+<p>As refer√™ncias bibliogr√°ficas atendem ao conte√∫do program√°tico?</p> 
+<p>Os objetivos desta forma√ß√£o ser√£o atingidos considerando o p√∫blico-alvo proposto? O corpo docente tem forma√ß√£o ou experi√™ncia na tem√°tica do curso?</p> 
+<p>No caso das forma√ß√µes a dist√¢ncia, a rela√ß√£o entre tutores e cursistas no ambiente virtual de aprendizagem permite a media√ß√£o satisfat√≥ria?</p>'
 where id = 1;
