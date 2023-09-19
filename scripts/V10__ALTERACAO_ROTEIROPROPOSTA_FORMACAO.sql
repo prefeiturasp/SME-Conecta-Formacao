543d6c394eebf594e7ee90c97e7f6a3b5ecb09cc
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-19 17:02:08 -0400

    Bug 104647: [101925 - Cadastro de propostas - Informações gerais (Listagem)] Ajuste na formatação de texto da modal acessada pelo link no object card

---
+++ scripts/V10__ALTERACAO_ROTEIROPROPOSTA_FORMACAO.sql
@@ -0,0 +1,15 @@
+ALTER TABLE roteiro_proposta_formativa ALTER COLUMN descricao TYPE text;
+
+update roteiro_proposta_formativa set descricao = '<p>Este roteiro tem como finalidade auxiliar na elaboração das propostas formativas no âmbito da Rede Municipal de Ensino.</p> 
+<p>O tema é prioritário para o desenvolvimento dos programas e projetos da SME?</p>
+<p>A carga horária apresentada está em acordo com o disposto no Edital NTF 2023?</p>
+<p>A justificativa apresenta diagnóstico da realidade local e/ou necessidade de continuidade ou aprofundamento no tema?</p> 
+<p>Há articulação entre objetivos, tema, metodologia, conteúdo, forma de abordagem e carga horária?</p> 
+<p>O conteúdo programático está alinhado a um ou mais princípios do Edital NTF 2023, com foco na melhoria das aprendizagens dos estudantes?</p> 
+<p>A carga horária possibilita a exploração do conteúdo apresentado de forma satisfatória, permitindo o aprofundamento na temática?</p> 
+<p>A metodologia e quantidade de participantes por turma são adequadas para promover a aquisição de saberes pelos cursistas e estão em acordo com o Edital NTF 2023?</p> 
+<p>A proposta tem como princípio metodológico o uso da problematização? Os procedimentos metodológicos favorecem a relação entre a teoria e a prática profissional?</p> 
+<p>As referências bibliográficas atendem ao conteúdo programático?</p> 
+<p>Os objetivos desta formação serão atingidos considerando o público-alvo proposto? O corpo docente tem formação ou experiência na temática do curso?</p> 
+<p>No caso das formações a distância, a relação entre tutores e cursistas no ambiente virtual de aprendizagem permite a mediação satisfatória?</p>'
+where id = 1;
