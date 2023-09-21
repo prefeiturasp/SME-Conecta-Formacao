6836bb7c82a39fadb328a9c35297ab858ce28619
Juliano Martins <juliano.campos@amcom.com.br>
2023-09-21 10:25:58 -0400

    Task 103220: Script para adicionar novos campos na tabela de proposta e encontros

---
+++ SME.ConectaFormacao.Dominio/Entidades/PropostaEncontroTurma.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.ConectaFormacao.Dominio.Entidades
+{
+    public class PropostaEncontroTurma : EntidadeBaseAuditavel
+    {
+        public long PropostaEncontroId { get; set; }
+        public short Turma { get; set; }
+    }
+}
