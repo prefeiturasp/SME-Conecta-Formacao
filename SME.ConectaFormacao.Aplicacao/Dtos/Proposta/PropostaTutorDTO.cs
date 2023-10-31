d6ac5d8ee9d7ddd3c52324bcd7543ad9247dfaea
Thiago Fernando <thiago.fernando@msn.com>
2023-10-31 08:26:04 -0300

    Ajustes de  campos não  obrigatorios

--- SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaTutorDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaTutorDTO.cs
@@ -5,7 +5,7 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Proposta
         public long Id { get; set; }
         public bool ProfissionalRedeMunicipal { get; set; }
         public string? RegistroFuncional { get; set; }
-        public string NomeTutor { get; set; }
+        public string? NomeTutor { get; set; }
         public string? NomesTurmas { get; set; }
         public IEnumerable<PropostaTutorTurmaDTO> Turmas { get; set; } = new List<PropostaTutorTurmaDTO>();
     }
