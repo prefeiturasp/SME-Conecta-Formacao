140b8739c8eb220353794df26dcdf99e20b6f3f0
Thiago Fernando <thiago.fernando@msn.com>
2023-11-09 10:59:05 -0300

    Ajuste no programa

--- SME.Conecta.Worker.Rabbbit/Program.cs
+++ SME.Conecta.Worker.Rabbbit/Program.cs
@@ -1,27 +1,4 @@
 //namespace SME.Conecta.Worker;
-
-//public class Program
-//{
-//    public static void Main(string[] args)
-//    {
-//        CreateHostBuilder(args).Build().Run();
-//    }
-
-//    public static IHostBuilder CreateHostBuilder(string[] args) =>
-//        Host.CreateDefaultBuilder(args)
-//            .ConfigureAppConfiguration((hostingContext, config) =>
-//            {
-//                config.AddEnvironmentVariables();
-//                config.AddUserSecrets<Program>();
-//            })
-//            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-//            .ConfigureServices(services =>
-//            {
-//                services.AddHostedService<WorkerRabbitMQ>();
-//                services.AddHealthChecks();
-//            });
-//}
-
 using Elastic.Apm.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
