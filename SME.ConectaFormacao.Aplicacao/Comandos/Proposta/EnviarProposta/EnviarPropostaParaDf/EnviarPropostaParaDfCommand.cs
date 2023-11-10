6aaafe089c188feb1881cb09e7a95a7230871219
Thiago Fernando <thiago.fernando@msn.com>
2023-11-10 13:57:04 -0300

    Criado command de enviar proposta para o DF

---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Proposta/EnviarProposta/EnviarPropostaParaDf/EnviarPropostaParaDfCommand.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class EnviarPropostaParaDfCommand : IRequest
+    {
+        public EnviarPropostaParaDfCommand(long propostaId)
+        {
+            PropostaId = propostaId;
+        }
+
+        public long PropostaId { get; set; }
+    }
+
+    public class EnviarPropostaParaDfCommandValidator : AbstractValidator<EnviarPropostaParaDfCommand>
+    {
+        public EnviarPropostaParaDfCommandValidator()
+        {
+            RuleFor(x => x.PropostaId).GreaterThan(0).WithMessage("Informe o Id da Proposta para enviar para o DF");
+        }
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Proposta/EnviarProposta/EnviarPropostaParaDf/EnviarPropostaParaDfCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class EnviarPropostaParaDfCommandHandler: IRequestHandler<EnviarPropostaParaDfCommand>
+    {
+        private readonly IRepositorioProposta _repositorioProposta;
+
+        public EnviarPropostaParaDfCommandHandler(IRepositorioProposta repositorioProposta)
+        {
+            _repositorioProposta = repositorioProposta ?? throw new ArgumentNullException(nameof(repositorioProposta));
+        }
+
+        public async Task Handle(EnviarPropostaParaDfCommand request, CancellationToken cancellationToken)
+        { 
+            await _repositorioProposta.EnviarPropostaParaDf(request.PropostaId);
+        }
+    }
+}
