ae57fee4d2e92eb07c0550e65e6bcde31c88520a
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-22 14:37:27 -0300

    Criação da tabela de justificativa/parecer de propostas

---
+++ scripts/V26__CRIAR_TABELA_PROPOSTA_JUSTIFICATIVA_PARECER.sql
@@ -0,0 +1,17 @@
+CREATE TABLE if not exists public.proposta_justificativa_parecer (
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY(NO MINVALUE NO MAXVALUE NO CYCLE),
+	proposta_id int8 not NULL,	
+	justificativa varchar(1000) NULL,
+	situacao smallint null,
+	criado_em timestamp NOT NULL,
+	criado_por varchar(200) NOT NULL,
+	alterado_em timestamp NULL,
+	alterado_por varchar(200) NULL,
+	criado_login varchar(200) NOT NULL,
+	alterado_login varchar(200) NULL,
+	excluido bool NOT NULL,
+	CONSTRAINT proposta_justificativa_parecer_pk PRIMARY KEY (id),
+	constraint proposta_justificativa_parecer_fk foreign key (proposta_id) references proposta (id)
+);
+CREATE INDEX if not exists proposta_justificativa_parecer_id_idx ON public.proposta_justificativa_parecer (id);
+CREATE INDEX if not exists proposta_justificativa_parecer_proposta_id_idx ON public.proposta_justificativa_parecer (proposta_id);
