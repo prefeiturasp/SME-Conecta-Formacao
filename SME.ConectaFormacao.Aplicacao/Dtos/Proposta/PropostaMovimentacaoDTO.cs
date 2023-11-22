3ad3fd283bed4cd30433cc8cde7b914940721edd
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-22 18:48:15 -0300

    Correção da mensagem de erro do DTO de entrada

--- SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaMovimentacaoDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaMovimentacaoDTO.cs
@@ -6,10 +6,10 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Proposta
     public class PropostaMovimentacaoDTO
     {
         [Required(ErrorMessage = "É necessário informar o parecer da proposta")]
-        [MaxLength(150, ErrorMessage = "O parecer da poroposta não pode conter mais que 1000 caracteres")]
+        [MaxLength(150, ErrorMessage = "O parecer da proposta não pode conter mais que 1000 caracteres")]
         public string Parecer { get; set; }
         
-        [Required(ErrorMessage = "É necessário informar a situação do parecer da proposta")]
+        [Required(ErrorMessage = "É necessário informar a situação da movimentação da proposta")]
         public SituacaoProposta Situacao { get; set; }
     }
 }
