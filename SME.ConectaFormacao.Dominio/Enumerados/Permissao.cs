73c551fa9b3ad8c93e148ffdd54cf97b8acb72bf
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-23 09:17:52 -0300

    [Back] Criar permissão no coresso - parecer

--- SME.ConectaFormacao.Dominio/Enumerados/Permissao.cs
+++ SME.ConectaFormacao.Dominio/Enumerados/Permissao.cs
@@ -6,9 +6,14 @@
         AreaPromotora_I = 286,
         AreaPromotora_E = 287,
         AreaPromotora_A = 288,
+        
         Proposta_C = 289,
         Proposta_I = 290,
         Proposta_E = 291,
         Proposta_A = 292,
+        
+        AtribuirPropostaParaGestao_I = 289,
+        DarParecerDaProposta_I = 290,
+        DevolverProposta_I = 291
     }
 }
