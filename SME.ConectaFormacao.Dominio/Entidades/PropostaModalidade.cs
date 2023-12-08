81a972b615239324adec1d2396e72de016d7cef7
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-12-08 09:35:56 -0300

    Criando proposta_modalidade, proposta_ano_turma e proposta_componente_curricular

---
+++ SME.ConectaFormacao.Dominio/Entidades/PropostaModalidade.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.ConectaFormacao.Dominio.Entidades
+{
+    public class PropostaModalidade : EntidadeBaseAuditavel
+    {
+        public long PropostaId { get; set; }
+        public int Modalidade { get; set; }
+    }
+}
