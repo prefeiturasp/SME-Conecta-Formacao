564d738851ad6121989d0f1d3413649bba949d61
Juliano Martins <juliano.campos@amcom.com.br>
2023-12-11 18:27:33 -0400

    Task 108513: [Back] Adicionar Cache no Redis

---
+++ SME.ConectaFormacao.Infra.Servicos/CacheDistribuido/Opcoes/CacheDistribuidoRedisOpcao.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+
+namespace SME.ConectaFormacao.Infra.Servicos.CacheDistribuido.Opcoes
+{
+    public class CacheDistribuidoRedisOpcao
+    {
+        public const string Secao = "Redis";
+
+        public string Endpoint { get; set; }
+        public Proxy Proxy { get; set; }
+        public int SyncTimeout { get; set; } = 5000;
+        public string Prefixo { get; set; } = "CONECTA:";
+    }
+}
