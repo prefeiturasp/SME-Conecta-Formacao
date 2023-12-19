8605e951131726e760c94c15885e3ac9ec755f2f
Juliano Martins <juliano.campos@amcom.com.br>
2023-12-19 08:35:44 -0400

    Bug 110200 - Correção listagem data realização

--- SME.ConectaFormacao.Aplicacao/Mapeamentos/DominioParaDTOProfile.cs
+++ SME.ConectaFormacao.Aplicacao/Mapeamentos/DominioParaDTOProfile.cs
@@ -89,7 +89,9 @@ namespace SME.ConectaFormacao.Aplicacao.Mapeamentos
                 .ForMember(dest => dest.TipoFormacao, opt => opt.MapFrom(x => x.TipoFormacao.HasValue ? x.TipoFormacao.Nome() : null))
                 .ForMember(dest => dest.Formato, opt => opt.MapFrom(x => x.Formato.HasValue ? x.Formato.Nome() : null))
                 .ForMember(dest => dest.Situacao, opt => opt.MapFrom(x => x.Situacao.Nome()))
-                .ForMember(dest => dest.AreaPromotora, opt => opt.MapFrom(x => x.AreaPromotora.Nome));
+                .ForMember(dest => dest.AreaPromotora, opt => opt.MapFrom(x => x.AreaPromotora.Nome))
+                .ForMember(dest => dest.DataRealizacaoInicio, opt => opt.MapFrom(x => x.DataRealizacaoInicio.HasValue ? x.DataRealizacaoInicio.Value.ToString("dd/MM/yyyy") : string.Empty))
+                .ForMember(dest => dest.DataRealizacaoFim, opt => opt.MapFrom(x => x.DataRealizacaoFim.HasValue ? x.DataRealizacaoFim.Value.ToString("dd/MM/yyyy") : string.Empty));
 
             CreateMap<Arquivo, PropostaImagemDivulgacaoDTO>()
                 .ForMember(dest => dest.ArquivoId, opt => opt.MapFrom(x => x.Id));
