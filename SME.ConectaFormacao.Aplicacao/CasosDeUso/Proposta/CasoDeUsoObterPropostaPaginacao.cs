46f193a1a9c28da1d81507d8825f1cccdfd8f9bb
Juliano Martins <juliano.campos@amcom.com.br>
2023-12-28 18:30:25 -0400

    Task 110062: Endpoint para lista inscrições

--- SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoObterPropostaPaginacao.cs
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoObterPropostaPaginacao.cs
@@ -2,21 +2,19 @@
 using SME.ConectaFormacao.Aplicacao.Dtos;
 using SME.ConectaFormacao.Aplicacao.Dtos.Proposta;
 using SME.ConectaFormacao.Aplicacao.Interfaces.Proposta;
+using SME.ConectaFormacao.Dominio.Contexto;
 
 namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Proposta
 {
-    public class CasoDeUsoObterPropostaPaginacao : CasoDeUsoAbstrato, ICasoDeUsoObterPropostaPaginacao
+    public class CasoDeUsoObterPropostaPaginacao : CasoDeUsoAbstratoPaginado, ICasoDeUsoObterPropostaPaginacao
     {
-        public CasoDeUsoObterPropostaPaginacao(IMediator mediator) : base(mediator)
+        public CasoDeUsoObterPropostaPaginacao(IMediator mediator, IContextoAplicacao contextoAplicacao) : base(mediator, contextoAplicacao)
         {
         }
 
         public async Task<PaginacaoResultadoDTO<PropostaPaginadaDTO>> Executar(PropostaFiltrosDTO propostaFiltrosDTO)
         {
-            int numeroPagina = int.TryParse(await mediator.Send(new ObterVariavelContextoAplicacaoQuery("NumeroPagina")), out numeroPagina) ? numeroPagina : 1;
-            int numeroRegistros = int.TryParse(await mediator.Send(new ObterVariavelContextoAplicacaoQuery("NumeroRegistros")), out numeroRegistros) ? numeroRegistros : 10;
-
-            return await mediator.Send(new ObterPropostaPaginadaQuery(propostaFiltrosDTO, numeroPagina, numeroRegistros));
+            return await mediator.Send(new ObterPropostaPaginadaQuery(propostaFiltrosDTO, NumeroPagina, NumeroRegistros));
         }
     }
 }
