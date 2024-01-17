eb52aa6baa5336f00eb279a681cbc787653f72d9
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-01-17 13:37:47 -0300

    Adicionando cache aos parâmetros

--- SME.ConectaFormacao.Aplicacao/Consultas/ParametrosSistema/ObterParametroSistemaPorTipoEAnoQueryHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Consultas/ParametrosSistema/ObterParametroSistemaPorTipoEAnoQueryHandler.cs
@@ -1,19 +1,27 @@
 ﻿using MediatR;
+using SME.ConectaFormacao.Dominio.Constantes;
 using SME.ConectaFormacao.Dominio.Entidades;
 using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
+using SME.ConectaFormacao.Infra.Servicos.Cache;
 
 namespace SME.ConectaFormacao.Aplicacao
 {
     public class ObterParametroSistemaPorTipoEAnoQueryHandler : IRequestHandler<ObterParametroSistemaPorTipoEAnoQuery, ParametroSistema>
     {
         private readonly IRepositorioParametroSistema repositorioParametroSistema;
+        private readonly ICacheDistribuido _cacheDistribuido;
 
-        public ObterParametroSistemaPorTipoEAnoQueryHandler(IRepositorioParametroSistema repositorioParametroSistema)
+        public ObterParametroSistemaPorTipoEAnoQueryHandler(IRepositorioParametroSistema repositorioParametroSistema,ICacheDistribuido cacheDistribuido)
         {
             this.repositorioParametroSistema = repositorioParametroSistema ?? throw new ArgumentNullException(nameof(repositorioParametroSistema));
+            _cacheDistribuido = cacheDistribuido ?? throw new ArgumentNullException(nameof(cacheDistribuido));
         }
 
-        public async Task<ParametroSistema> Handle(ObterParametroSistemaPorTipoEAnoQuery request, CancellationToken cancellationToken)
-            => await repositorioParametroSistema.ObterParametroPorTipoEAno(request.TipoParametroSistema, request.Ano);
+        public async Task<ParametroSistema> Handle(ObterParametroSistemaPorTipoEAnoQuery request,
+            CancellationToken cancellationToken)
+        {
+            var chave = string.Format(CacheDistribuidoNomes.ParametroSistemaTipo, request.TipoParametroSistema);
+            return await _cacheDistribuido.ObterAsync( chave, () => repositorioParametroSistema.ObterParametroPorTipoEAno(request.TipoParametroSistema, request.Ano));
+        }
     }
 }
--- SME.ConectaFormacao.Dominio/Constantes/CacheDistribuidoNomes.cs
+++ SME.ConectaFormacao.Dominio/Constantes/CacheDistribuidoNomes.cs
@@ -10,6 +10,7 @@
         /// 0 - Tipo
         /// </summary>
         public const string CargoFuncao = "cargo-funcao:{0}";
+        public const string ParametroSistemaTipo = "parametro-sistema-tipo:{0}";
         public const string AreaPromotora = "area-promotora";
         public const string PalavraChave = "palavra-chave";
         /// <summary>
