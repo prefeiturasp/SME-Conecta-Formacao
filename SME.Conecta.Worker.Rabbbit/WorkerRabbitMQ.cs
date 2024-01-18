13d619d6dc96a5b6c336ef1de232e2d357892073
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-01-18 11:09:24 -0300

    Ajustar as legendas de cada fila

--- SME.Conecta.Worker.Rabbbit/WorkerRabbitMQ.cs
+++ SME.Conecta.Worker.Rabbbit/WorkerRabbitMQ.cs
@@ -35,8 +35,8 @@ namespace SME.Conecta.Worker
             
             Comandos.Add(RotasRabbit.RealizarInscricaoAutomatica, new ComandoRabbit("Realizar Inscrições Automáticas - Obter Formações e Cursistas", typeof(ICasoDeUsoRealizarInscricaoAutomatica), true));
             Comandos.Add(RotasRabbit.RealizarInscricaoAutomaticaTratarTurmas, new ComandoRabbit("Realizar Inscrições Automáticas - Tratar as turmas - incluir novas conforme a quantidade de cursistas", typeof(ICasoDeUsoRealizarInscricaoAutomaticaTratar), true));
-            Comandos.Add(RotasRabbit.RealizarInscricaoAutomaticaTratarCursistas, new ComandoRabbit("Realizar Inscrições Automáticas - Iterar sobre os cursistas e coletar o usuário logado", typeof(ICasoDeUsoRealizarInscricaoAutomaticaTratarCursista), true));
-            Comandos.Add(RotasRabbit.RealizarInscricaoAutomaticaIncreverCursista, new ComandoRabbit("Realizar Inscrições Automáticas - Iterar sobre os cursistas e coletar o usuário logado", typeof(ICasoDeUsoRealizarInscricaoAutomaticaInscreverCursista), true));
+            Comandos.Add(RotasRabbit.RealizarInscricaoAutomaticaTratarCursistas, new ComandoRabbit("Realizar Inscrições Automáticas - Iterar sobre os cursistas e enviar para fila de inscrição", typeof(ICasoDeUsoRealizarInscricaoAutomaticaTratarCursista), true));
+            Comandos.Add(RotasRabbit.RealizarInscricaoAutomaticaIncreverCursista, new ComandoRabbit("Realizar Inscrições Automáticas - Coletar usuário e inscrever cursista", typeof(ICasoDeUsoRealizarInscricaoAutomaticaInscreverCursista), true));
         }
     }
 }
