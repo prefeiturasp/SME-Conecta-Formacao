a292a1deda9361b482f06889937d6839cef5731a
Juliano Martins <juliano.campos@amcom.com.br>
2024-01-23 18:18:12 -0400

    ajustes

--- SME.ConectaFormacao.Aplicacao/Dtos/Inscricao/InscricaoAutomaticaTurmaPropostaDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SME.ConectaFormacao.Aplicacao.Dtos.Inscricao
-{
-    public class InscricaoAutomaticaTurmaPropostaDTO
-    {
-        public long Id { get; set; }
-        public string CodigoDre { get; set; }
-    }
-}
