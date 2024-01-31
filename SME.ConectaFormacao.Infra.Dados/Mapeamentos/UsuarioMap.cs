2ba2b5de0fb4cbee69bfa2b29572395334a9f229
Thiago Fernando <thiago.fernando@msn.com>
2024-01-31 10:31:38 -0300

    Adicionar campos tabela de usuario - domínio - repositório - mapeamento

--- SME.ConectaFormacao.Infra.Dados/Mapeamentos/UsuarioMap.cs
+++ SME.ConectaFormacao.Infra.Dados/Mapeamentos/UsuarioMap.cs
@@ -14,6 +14,10 @@ namespace SME.ConectaFormacao.Infra.Dados.Mapeamentos
             Map(c => c.TokenRecuperacaoSenha).ToColumn("token_recuperacao_senha");
             Map(c => c.ExpiracaoRecuperacaoSenha).ToColumn("expiracao_recuperacao_senha");
             Map(c => c.Cpf).ToColumn("cpf");
+            Map(c => c.UeCodigo).ToColumn("ue_codigo");
+            Map(c => c.Tipo).ToColumn("tipo");
+            Map(c => c.PossuiContratoExterno).ToColumn("possui_contrato_externo");
+            Map(c => c.Situacao).ToColumn("situacao_cadastro");
         }
     }
 }
---
+++ scripts/V46__INSERIR_PARAMETRO_SISTEMA_DOMINIO_EMAIL.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.parametro_sistema
+(id, nome, tipo, descricao, valor, ano, ativo, criado_em, criado_por, alterado_em, alterado_por, criado_login, alterado_login, excluido)
+VALUES(NULL, 'DominioPermitidoCadastroUsuarioExterno', 4, 'Domínio Permitido para Cadastro de Usuário Externo', '@edu.sme.prefeitura.sp.gov.br', 2024, NULL, now(), 'Sistema', NULL, NULL, 'Sistema', NULL, NULL);
