b5eb94146d273baaf7d1d7ca470cd0586ee8f808
Thiago Fernando <thiago.fernando@msn.com>
2024-02-02 15:09:17 -0300

    Ajustes endpoint

--- SME.ConectaFormacao.Webapi/Controllers/FuncionarioExternoController.cs
+++ SME.ConectaFormacao.Webapi/Controllers/FuncionarioExternoController.cs
@@ -7,7 +7,7 @@ using SME.ConectaFormacao.Infra.Servicos.Eol;
 
 namespace SME.ConectaFormacao.Webapi.Controllers
 {
-    
+    [Authorize("Bearer")]
     public class FuncionarioExternoController : BaseController
     {
         [HttpGet("{cpf}")]
