3edeb4027fd6bfe6c45ad6a0179ec56ed7da9303
Thiago Fernando <thiago.fernando@msn.com>
2024-02-09 13:48:48 -0300

    ajuste de nomenclatura

--- SME.ConectaFormacao.Webapi/Controllers/UnidadeEolController.cs
+++ SME.ConectaFormacao.Webapi/Controllers/UnidadeEolController.cs
@@ -7,16 +7,16 @@ namespace SME.ConectaFormacao.Webapi.Controllers
 {
     public class UnidadeEolController : BaseController
     {
-        [HttpGet("{codigoEOl}")]
+        [HttpGet("{codigoEol}")]
         [ProducesResponseType(typeof(UnidadeEol), 200)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
         public async Task<IActionResult> BuscarUnidadePorCodigoEol(
-            [FromRoute] string codigoEOl,
+            [FromRoute] string codigoEol,
             [FromServices] ICasoDeUsoObterUnidadePorCodigoEol useCase
             )
         {
-            return Ok(await useCase.Executar(codigoEOl));
+            return Ok(await useCase.Executar(codigoEol));
         }
     }
 }
