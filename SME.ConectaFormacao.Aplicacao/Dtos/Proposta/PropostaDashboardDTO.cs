0d75384cf074416ad7db88adf887e9e2be271831
Thiago Fernando <thiago.fernando@msn.com>
2024-02-09 14:45:02 -0300

    Adicionado HabilitarVerMais

--- SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaDashboardDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaDashboardDTO.cs
@@ -8,7 +8,7 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Proposta
         }
         public string? Situacao { get; set; }
         public string? Cor { get; set; }
-        public bool VerMais { get; set; }
+        public bool HabilitarVerMais { get; set; }
         public int TotalRegistros { get; set; }
         public List<PropostaDashboardItemDTO> Propostas { get; set; }
     }
