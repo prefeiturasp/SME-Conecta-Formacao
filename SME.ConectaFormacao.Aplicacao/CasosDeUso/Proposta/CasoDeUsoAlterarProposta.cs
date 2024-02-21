84c2022a04ed3616a485a0f92a32ec99cb7a37cf
Thiago Fernando <thiago.fernando@msn.com>
2024-02-21 16:41:01 -0300

    Remover Validação de email

--- SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoAlterarProposta.cs
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Proposta/CasoDeUsoAlterarProposta.cs
@@ -9,7 +9,6 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Proposta
 {
     public class CasoDeUsoAlterarProposta : CasoDeUsoAbstrato, ICasoDeUsoAlterarProposta
     {
-        const long CODIGO_DRE_TODOS = 14;
         public CasoDeUsoAlterarProposta(IMediator mediator) : base(mediator)
         {
         }
@@ -19,18 +18,8 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Proposta
 
             if (propostaDTO.Situacao == Dominio.Enumerados.SituacaoProposta.Rascunho)
                 return await mediator.Send(new AlterarPropostaRascunhoCommand(id, propostaDTO));
-
-            var existeDreTodos = propostaDTO.Turmas.Select(x => x.DresIds).Where(x => x.Contains(CODIGO_DRE_TODOS)).Count();
-
-            if(existeDreTodos > 0)
-                throw new NegocioException(MensagemNegocio.DRE_NAO_INFORMADA_PARA_TODAS_AS_TURMAS);
-
-            await SalvarMovimentacao(id, propostaDTO.Situacao);
             return await mediator.Send(new AlterarPropostaCommand(id, propostaDTO));
         }
-        private async Task SalvarMovimentacao(long propostaId, SituacaoProposta situacao)
-        {
-            await mediator.Send(new SalvarPropostaMovimentacaoCommand(propostaId, situacao));
-        }
+
     }
 }
--- SME.ConectaFormacao.Aplicacao/Comandos/Proposta/AlterarProposta/AlterarPropostaCommandHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Comandos/Proposta/AlterarProposta/AlterarPropostaCommandHandler.cs
@@ -27,6 +27,11 @@ namespace SME.ConectaFormacao.Aplicacao
 
         public async Task<long> Handle(AlterarPropostaCommand request, CancellationToken cancellationToken)
         {
+            var existeDreTodos = request.PropostaDTO.Turmas.Select(x => x.DresIds).Count(x => x.Contains(DRE_ID_TODOS));
+
+            if(existeDreTodos > 0)
+                throw new NegocioException(MensagemNegocio.DRE_NAO_INFORMADA_PARA_TODAS_AS_TURMAS);
+            
             var proposta = await _repositorioProposta.ObterPorId(request.Id) ?? throw new NegocioException(MensagemNegocio.PROPOSTA_NAO_ENCONTRADA, System.Net.HttpStatusCode.NotFound);
 
             await _mediator.Send(new ValidarFuncaoEspecificaOutrosCommand(request.PropostaDTO.FuncoesEspecificas, request.PropostaDTO.FuncaoEspecificaOutros), cancellationToken);
@@ -87,9 +92,8 @@ namespace SME.ConectaFormacao.Aplicacao
                 await _repositorioProposta.Atualizar(propostaDepois);
 
                 await _mediator.Send(new SalvarPropostaCommand(propostaDepois.Id, propostaDepois, proposta.ArquivoImagemDivulgacaoId), cancellationToken);
-
+                await SalvarMovimentacao(request.Id,request.PropostaDTO.Situacao);
                 transacao.Commit();
-
                 return request.Id;
             }
             catch
@@ -101,6 +105,10 @@ namespace SME.ConectaFormacao.Aplicacao
             {
                 transacao.Dispose();
             }
+        }
+        private async Task SalvarMovimentacao(long propostaId, SituacaoProposta situacao)
+        {
+            await _mediator.Send(new SalvarPropostaMovimentacaoCommand(propostaId, situacao));
         }
     }
 }
