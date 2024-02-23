1a95977b878778f9cf82aab9139a6a108cabc2e8
Juliano Martins <juliano.campos@amcom.com.br>
2024-02-23 15:01:16 -0400

    Bug 115798: [PROD] Inscrições automáticas indevidas

--- SME.ConectaFormacao.Aplicacao/CasosDeUso/Inscricao/CasoDeUsoRealizarInscricaoAutomatica.cs
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Inscricao/CasoDeUsoRealizarInscricaoAutomatica.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
 using SME.ConectaFormacao.Aplicacao.Dtos.Inscricao;
 using SME.ConectaFormacao.Aplicacao.Interfaces.Inscricao;
+using SME.ConectaFormacao.Dominio.Constantes;
 using SME.ConectaFormacao.Dominio.Enumerados;
+using SME.ConectaFormacao.Dominio.Extensoes;
 using SME.ConectaFormacao.Infra;
 
 namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Inscricao
@@ -20,11 +22,17 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Inscricao
             if (propostaInscricaoAutomatica.Situacao != SituacaoProposta.Publicada || !propostaInscricaoAutomatica.EhInscricaoAutomatica)
                 return false;
 
+            if (propostaInscricaoAutomatica.PublicosAlvos.PossuiElementos() && propostaInscricaoAutomatica.PublicosAlvos.Any(t => !t.HasValue))
+                throw new Exception(MensagemNegocio.PROPOSTA_COM_PUBLICO_ALVO_SEM_DEPARA_CONFIGURADO.Parametros(propostaId));
+
+            if (propostaInscricaoAutomatica.FuncoesEspecificas.PossuiElementos() && propostaInscricaoAutomatica.FuncoesEspecificas.Any(t => !t.HasValue))
+                throw new Exception(MensagemNegocio.PROPOSTA_COM_FUNCAO_ESPECIFICA_SEM_DEPARA_CONFIGURADO.Parametros(propostaId));
+
             await mediator.Send(new GerarPropostaTurmaVagaCommand(propostaId, propostaInscricaoAutomatica.QuantidadeVagasTurmas));
 
             var cursistasEOL = await mediator.Send(new ObterFuncionarioPorFiltroPropostaServicoEolQuery(
-                propostaInscricaoAutomatica.PublicosAlvos,
-                propostaInscricaoAutomatica.FuncoesEspecificas,
+                propostaInscricaoAutomatica.PublicosAlvos.Select(t => t.Value),
+                propostaInscricaoAutomatica.FuncoesEspecificas.Select(t => t.Value),
                 propostaInscricaoAutomatica.Modalidades,
                 propostaInscricaoAutomatica.AnosTurmas,
                 propostaInscricaoAutomatica.PropostasTurmas.Select(turma => turma.CodigoDre).Distinct(),
