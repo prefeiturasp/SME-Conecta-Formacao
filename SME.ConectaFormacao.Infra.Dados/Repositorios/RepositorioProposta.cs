be684fe1f2edb4db6911e7a76cb6b42c1beadf69
Juliano Martins <juliano.campos@amcom.com.br>
2024-02-25 16:50:42 -0400

    correção turma sem dre

--- SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioProposta.cs
+++ SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioProposta.cs
@@ -2041,7 +2041,7 @@ namespace SME.ConectaFormacao.Infra.Dados.Repositorios
                    dre.dre_id as codigoDre
             from proposta_turma pt
               join proposta_turma_dre ptd on ptd.proposta_turma_id = pt.id and not ptd.excluido 
-              join dre on dre.id = ptd.dre_id and not dre.excluido and not dre.todos
+              left join dre on dre.id = ptd.dre_id and not dre.excluido and not dre.todos
             where not pt.excluido 
               and pt.proposta_id = @propostaId;
               
