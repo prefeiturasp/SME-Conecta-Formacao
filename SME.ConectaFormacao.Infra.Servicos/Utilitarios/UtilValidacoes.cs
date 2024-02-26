10b204e3c4b8dc75cafe5224ee1448ab55220783
Thiago Fernando <thiago.fernando@msn.com>
2024-02-26 11:19:11 -0300

    Cadastro de usuários - Não está validando CPF

--- SME.ConectaFormacao.Infra.Servicos/Utilitarios/UtilValidacoes.cs
+++ SME.ConectaFormacao.Infra.Servicos/Utilitarios/UtilValidacoes.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using SME.ConectaFormacao.Dominio.Extensoes;
 
 namespace SME.ConectaFormacao.Infra.Servicos.Utilitarios
 {
@@ -16,44 +17,42 @@ namespace SME.ConectaFormacao.Infra.Servicos.Utilitarios
 
         public static bool CpfEhValido(string cpf)
         {
-            int[] multiplicador1 = new int[9] {10, 9, 8, 7, 6, 5, 4, 3, 2};
-            int[] multiplicador2 = new int[10] {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", "");
-
+            cpf = cpf.SomenteNumeros();
             if (cpf.Length != 11)
                 return false;
 
-            string tempCpf = cpf.Substring(0, 9);
-            int soma = 0;
+            char primeiroDigito = cpf[0];
+            if(cpf == string.Empty.PadLeft(11, primeiroDigito))
+                return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf, digito;
+            int soma = 0, resto;
+            tempCpf = cpf[..9];
 
             for (int i = 0; i < 9; i++)
                 soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
 
-            int resto = soma % 11;
-
+            resto = soma % 11;
             if (resto < 2)
                 resto = 0;
             else
                 resto = 11 - resto;
 
-            string digito = resto.ToString();
-            tempCpf = tempCpf + digito;
-            soma = 0;
+            digito = resto.ToString();
+            tempCpf += digito;
 
+            soma = 0;
             for (int i = 0; i < 10; i++)
                 soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
 
             resto = soma % 11;
-
             if (resto < 2)
                 resto = 0;
             else
                 resto = 11 - resto;
-
-            digito = digito + resto.ToString();
-
+            digito += resto.ToString();
             return cpf.EndsWith(digito);
         }
     }
