e1e7d673f5a03eceacdfc3403db4a21067c76de8
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-03-04 18:50:37 -0300

    Incluindo teste de integração

---
+++ SME.ConectaFormacao.TesteIntegracao/CasosDeUso/Usuario/Ao_alterar_nome_do_usuario.cs
@@ -0,0 +1,51 @@
+﻿using Shouldly;
+using SME.ConectaFormacao.Aplicacao.Interfaces.Usuario;
+using SME.ConectaFormacao.Dominio.Excecoes;
+using SME.ConectaFormacao.TesteIntegracao.CasosDeUso.Usuario.Mocks;
+using SME.ConectaFormacao.TesteIntegracao.Setup;
+using Xunit;
+
+namespace SME.ConectaFormacao.TesteIntegracao.CasosDeUso.Usuario
+{
+    public class Ao_alterar_nome_do_usuario : TesteBase
+    {
+        public Ao_alterar_nome_do_usuario(CollectionFixture collectionFixture) : base(collectionFixture, false)
+        {
+            UsuarioAlterarNomeMock.Montar();
+        }
+
+        [Fact(DisplayName = "Usuário - Deve retornar exceção ao alterar com nome não preenchido")]
+        public async Task Deve_retornar_excecao_ao_alterar_com_nome_nao_preenchido()
+        {
+            // arrange
+            var login = UsuarioAlterarNomeMock.Login;
+            var nome = UsuarioAlterarNomeMock.NomeInvalido;
+
+            var casoDeUso = ObterCasoDeUso<ICasoDeUsoUsuarioAlterarNome>();
+
+            // act
+            var excecao = await Should.ThrowAsync<NegocioException>(casoDeUso.Executar(login, nome));
+
+            // assert
+            excecao.ShouldNotBeNull();
+            excecao.StatusCode.ShouldBe(400);
+            excecao.Mensagens.Contains("É necessário informar o novo nome para ser alterado do usuário");
+        }
+
+        [Fact(DisplayName = "Usuário - Deve alterar o nome do usuário")]
+        public async Task Deve_alterar_o_nome_do_usuario()
+        {
+            // arrange
+            var login = UsuarioAlterarNomeMock.Login;
+            var nome = UsuarioAlterarNomeMock.NomeValido;
+
+            var casoDeUso = ObterCasoDeUso<ICasoDeUsoUsuarioAlterarNome>();
+
+            // act
+            var retorno = await casoDeUso.Executar(login, nome);
+
+            // assert
+            retorno.ShouldBeTrue();
+        }
+    }
+}
---
+++ SME.ConectaFormacao.TesteIntegracao/ServicosFakes/AlterarNomeServicoAcessosCommandHandlerFake.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Aplicacao;
+
+namespace SME.ConectaFormacao.TesteIntegracao.ServicosFakes
+{
+    public class AlterarNomeServicoAcessosCommandHandlerFake : IRequestHandler<AlterarNomeServicoAcessosCommand, bool>
+    {
+        public Task<bool> Handle(AlterarNomeServicoAcessosCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
