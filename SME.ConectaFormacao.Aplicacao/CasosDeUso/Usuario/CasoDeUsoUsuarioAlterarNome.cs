ea384464aa0fc0e89eb81dbb7c8e13d5f43de273
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-03-04 18:22:55 -0300

    [BACK] Criar endpoint para Alterar o nome do usuário no CORESSO

---
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/Usuario/CasoDeUsoUsuarioAlterarNome.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Aplicacao.Interfaces.Usuario;
+
+namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.Usuario
+{
+    public class CasoDeUsoUsuarioAlterarNome : CasoDeUsoAbstrato, ICasoDeUsoUsuarioAlterarNome
+    {
+        public CasoDeUsoUsuarioAlterarNome(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar(string login, string email)
+        {
+            return await mediator.Send(new AlterarNomeServicoAcessosCommand(login, email));
+        }
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Interfaces/Usuario/ICasoDeUsoUsuarioAlterarNome.cs
@@ -0,0 +1,7 @@
+﻿namespace SME.ConectaFormacao.Aplicacao.Interfaces.Usuario
+{
+    public interface ICasoDeUsoUsuarioAlterarNome
+    {
+        Task<bool> Executar(string login, string nome);
+    }
+}
