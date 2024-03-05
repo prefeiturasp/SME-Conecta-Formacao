6399991a2b1193e4dd76bdcff10df7661a249c40
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-03-05 09:25:54 -0300

    Removendo a máscara do cpf

--- SME.ConectaFormacao.TesteIntegracao/CasosDeUso/Usuario/Mocks/UsuarioInserirExternoMock.cs
+++ SME.ConectaFormacao.TesteIntegracao/CasosDeUso/Usuario/Mocks/UsuarioInserirExternoMock.cs
@@ -11,7 +11,7 @@ public class UsuarioInserirExternoMock
     public static UsuarioExternoDTO GerarUsuarioExternoDTO()
     {
         var pessoa = new Person("pt_BR");
-        var cpf = pessoa.Cpf().Limite(11);
+        var cpf = pessoa.Cpf().Replace(".","").Replace("-","");
         var senha = "Minha@Senha1";
         return new UsuarioExternoDTO
         {
