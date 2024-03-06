ac485841b842cfb03464272060b67148e39c536e
Juliano Martins <juliano.campos@amcom.com.br>
2024-03-06 16:53:39 -0400

    renomeando rota

--- SME.ConectaFormacao.Infra.Servicos/Acessos/Constante/EndpointsServicoAcessosConstantes.cs
+++ SME.ConectaFormacao.Infra.Servicos/Acessos/Constante/EndpointsServicoAcessosConstantes.cs
@@ -7,7 +7,7 @@
         public const string URL_AUTENTICACAO_USUARIOS_X_SISTEMAS_Y_PERFIS = "v1/autenticacao/usuarios/{0}/sistemas/{1}/perfis";
         public const string URL_AUTENTICACAO_USUARIOS_X_SISTEMAS_Y_PERFIS_Z = "v1/autenticacao/usuarios/{0}/sistemas/{1}/perfis/{2}";
         public const string URL_USUARIOS_X_CADASTRADO = "v1/usuarios/{0}/cadastrado";
-        public const string URL_USUARIOS_ATUALIZAR = "v1/usuarios/{0}";
+        public const string URL_USUARIOS_ATUALIZAR = "v1/usuarios/{0}/atualizar";
         public const string URL_USUARIOS_CADASTRAR = "v1/usuarios/cadastrar";
         public const string URL_USUARIOS_X_VINCULAR_PERFIL_Y = "v1/usuarios/{0}/vincular-perfil/{1}";
         public const string URL_USUARIOS_X = "v1/usuarios/{0}";
