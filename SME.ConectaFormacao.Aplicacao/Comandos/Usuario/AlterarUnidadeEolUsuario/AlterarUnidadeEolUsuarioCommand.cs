6d472fe76bcb15cca1ca74c67ec69e4ae09e51f2
Juliano Martins <juliano.campos@amcom.com.br>
2024-03-07 17:12:55 -0400

    Task 116281: [BACK] Criar endpoint para alterar unidade do usuário logado - critério 12

---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Usuario/AlterarUnidadeEolUsuario/AlterarUnidadeEolUsuarioCommand.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class AlterarUnidadeEolUsuarioCommand : IRequest<bool>
+    {
+        public AlterarUnidadeEolUsuarioCommand(string login, string codigoEolUnidade)
+        {
+            Login = login;
+            CodigoEolUnidade = codigoEolUnidade;
+        }
+
+        public string Login { get; }
+        public string CodigoEolUnidade { get; set; }
+    }
+
+    public class AlterarUnidadeEolUsuarioCommandValidator : AbstractValidator<AlterarUnidadeEolUsuarioCommand>
+    {
+        public AlterarUnidadeEolUsuarioCommandValidator()
+        {
+            RuleFor(r => r.Login).NotEmpty().WithMessage("Informe o login do usuário para realizar a alteração da unidade eol");
+            RuleFor(r => r.CodigoEolUnidade).NotEmpty().WithMessage("Informe o código da unidade do usuário para realizar a alteração da unidade eol");
+        }
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Usuario/AlterarUnidadeEolUsuario/AlterarUnidadeEolUsuarioCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Dominio.Constantes;
+using SME.ConectaFormacao.Dominio.Excecoes;
+using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class AlterarUnidadeEolUsuarioCommandHandler : IRequestHandler<AlterarUnidadeEolUsuarioCommand, bool>
+    {
+        private readonly IRepositorioUsuario _repositorioUsuario;
+
+        public AlterarUnidadeEolUsuarioCommandHandler(IRepositorioUsuario repositorioUsuario)
+        {
+            _repositorioUsuario = repositorioUsuario ?? throw new ArgumentNullException(nameof(repositorioUsuario));
+        }
+
+        public async Task<bool> Handle(AlterarUnidadeEolUsuarioCommand request, CancellationToken cancellationToken)
+        {
+            var usuario = await _repositorioUsuario.ObterPorLogin(request.Login) ?? 
+                throw new NegocioException(MensagemNegocio.USUARIO_NAO_ENCONTRADO);
+
+            usuario.CodigoEolUnidade = request.CodigoEolUnidade;
+            await _repositorioUsuario.Atualizar(usuario);
+
+            return true;
+        }
+    }
+}
