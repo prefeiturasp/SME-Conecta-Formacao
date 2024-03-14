e149767fe1769d251b555552e91848868f1e50f0
Thiago Fernando <thiago.fernando@msn.com>
2024-03-14 07:37:22 -0300

    correção critério 3 remover cache ao alterar unidade do usuario

--- SME.ConectaFormacao.Aplicacao/Comandos/Usuario/AlterarUnidadeEolUsuario/AlterarUnidadeEolUsuarioCommandHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Comandos/Usuario/AlterarUnidadeEolUsuario/AlterarUnidadeEolUsuarioCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using SME.ConectaFormacao.Dominio.Constantes;
 using SME.ConectaFormacao.Dominio.Excecoes;
+using SME.ConectaFormacao.Dominio.Extensoes;
 using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
 
 namespace SME.ConectaFormacao.Aplicacao
@@ -8,10 +9,12 @@ namespace SME.ConectaFormacao.Aplicacao
     public class AlterarUnidadeEolUsuarioCommandHandler : IRequestHandler<AlterarUnidadeEolUsuarioCommand, bool>
     {
         private readonly IRepositorioUsuario _repositorioUsuario;
+        private readonly IMediator _mediator; 
 
-        public AlterarUnidadeEolUsuarioCommandHandler(IRepositorioUsuario repositorioUsuario)
+        public AlterarUnidadeEolUsuarioCommandHandler(IRepositorioUsuario repositorioUsuario,IMediator mediator)
         {
             _repositorioUsuario = repositorioUsuario ?? throw new ArgumentNullException(nameof(repositorioUsuario));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         public async Task<bool> Handle(AlterarUnidadeEolUsuarioCommand request, CancellationToken cancellationToken)
@@ -23,6 +26,11 @@ namespace SME.ConectaFormacao.Aplicacao
             await _repositorioUsuario.Atualizar(usuario);
 
             return true;
+        }
+        private async Task RemoverCache(string login, string codigoUnidade)
+        {
+            await _mediator.Send(new RemoverCacheCommand(CacheDistribuidoNomes.UsuarioLogado.Parametros(login)));
+            await _mediator.Send(new RemoverCacheCommand(CacheDistribuidoNomes.Usuario.Parametros(login)));
         }
     }
 }
