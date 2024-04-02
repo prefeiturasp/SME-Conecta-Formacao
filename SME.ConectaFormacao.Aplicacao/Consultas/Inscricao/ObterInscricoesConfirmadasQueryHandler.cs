77cd585e4c025c80d9159fce8ffe90165391171f
fernando.groeler <fernando.groeler@gmail.com>
2024-04-02 08:58:59 -0400

    Ajustada classe

--- SME.ConectaFormacao.Aplicacao/Consultas/Inscricao/ObterInscricoesConfirmadasQueryHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Consultas/Inscricao/ObterInscricoesConfirmadasQueryHandler.cs
@@ -2,19 +2,22 @@
 using SME.ConectaFormacao.Dominio.Entidades;
 using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
 
-namespace SME.ConectaFormacao.Aplicacao;
-
-public class ObterInscricoesConfirmadasQueryHandler : IRequestHandler<ObterInscricoesConfirmadasQuery, IEnumerable<Inscricao>>
+namespace SME.ConectaFormacao.Aplicacao
 {
-    private readonly IRepositorioInscricao _repositorioInscricao;
-
-    public ObterInscricoesConfirmadasQueryHandler(IRepositorioInscricao repositorioInscricao)
+    public class ObterInscricoesConfirmadasQueryHandler : IRequestHandler<ObterInscricoesConfirmadasQuery, IEnumerable<Inscricao>>
     {
-        _repositorioInscricao = repositorioInscricao ?? throw new ArgumentNullException(nameof(repositorioInscricao));
-    }
+        private readonly IRepositorioInscricao _repositorioInscricao;
 
-    public async Task<IEnumerable<Inscricao>> Handle(ObterInscricoesConfirmadasQuery request, CancellationToken cancellationToken)
-    {
-        return await _repositorioInscricao.ObterInscricoesConfirmadas();
+        public ObterInscricoesConfirmadasQueryHandler(IRepositorioInscricao repositorioInscricao)
+        {
+            _repositorioInscricao =
+                repositorioInscricao ?? throw new ArgumentNullException(nameof(repositorioInscricao));
+        }
+
+        public async Task<IEnumerable<Inscricao>> Handle(ObterInscricoesConfirmadasQuery request,
+            CancellationToken cancellationToken)
+        {
+            return await _repositorioInscricao.ObterInscricoesConfirmadas();
+        }
     }
 }
