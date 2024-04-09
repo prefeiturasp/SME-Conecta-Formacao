8e91b966071139c882f1d8c8970c85494ca58534
Thiago Fernando <thiago.fernando@msn.com>
2024-04-09 15:07:38 -0300

    Ao alterar nome não altera o e-mail @edu

---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Usuario/AlterarEmailEduAoAlterarNome/AlterarEmailEduAoAlterarNomeCommand.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using MediatR;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class AlterarEmailEduAoAlterarNomeCommand : IRequest<bool>
+    {
+        public AlterarEmailEduAoAlterarNomeCommand(string login)
+        {
+            Login = login;
+        }
+        
+        public string Login { get; set; }
+    }
+
+    public class AlterarEmailEduAoAlterarNomeCommandValidator : AbstractValidator<AlterarEmailEduAoAlterarNomeCommand>
+    {
+        public AlterarEmailEduAoAlterarNomeCommandValidator()
+        {
+            RuleFor(x => x.Login).NotNull().NotEmpty().WithMessage("Informe o login para alterar o e-mail @edu");
+        }
+    }
+}
