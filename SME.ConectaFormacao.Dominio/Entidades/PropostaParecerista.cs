5684e657486fcdaa6db3ae01b702d70ebcc4511b
Thiago Fernando <thiago.fernando@msn.com>
2024-04-22 14:20:18 -0300

    Criar script e tabela proposta parecerista

---
+++ SME.ConectaFormacao.Dominio/Entidades/PropostaParecerista.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SME.ConectaFormacao.Dominio.Entidades
+{
+    public class PropostaParecerista : EntidadeBaseAuditavel
+    {
+
+        public long PropostaId { get; set; }
+        public string? RegistroFuncional { get; set; }
+        public string? NomeParecerista { get; set; }
+    }
+}
