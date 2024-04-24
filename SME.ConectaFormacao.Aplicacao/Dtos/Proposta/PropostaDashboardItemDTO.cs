2f9027cf64c2f95e98167b56a61cb657ae4a3e44
DanylloPereira <danyllo.antunes@gmail.com>
2024-04-24 15:17:47 -0300

    119784 - Apresentar formações com inscrição externa

--- SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaDashboardItemDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaDashboardItemDTO.cs
@@ -5,5 +5,7 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Proposta
         public long Numero { get; set; }
         public string? Nome { get; set; }
         public string? Data { get; set; }
+        public string? LinkParaInscricoesExterna { get; set; }
+        public bool PodeEnviarInscricao { get; set; }
     }
 }
