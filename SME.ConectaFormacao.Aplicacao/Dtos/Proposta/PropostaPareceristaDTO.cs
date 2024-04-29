bdd5804f74e5b62c4b3cf7ab49094568b5376334
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-04-29 11:59:07 -0300

    PropostaPareceristaDTO não é obrigatório em todos os momentos

--- SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaPareceristaDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Proposta/PropostaPareceristaDTO.cs
@@ -2,8 +2,8 @@ namespace SME.ConectaFormacao.Aplicacao
 {
     public class PropostaPareceristaDTO
     {
-        public long Id { get; set; }
-        public string RegistroFuncional { get; }
-        public string NomeParecerista { get; }
+        public long? Id { get; set; }
+        public string? RegistroFuncional { get; }
+        public string? NomeParecerista { get; }
     }
 }
