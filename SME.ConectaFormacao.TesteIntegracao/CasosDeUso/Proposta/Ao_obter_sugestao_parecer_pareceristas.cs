047074eb523ae0f60bed0534214034809c3eb09b
Juliano Martins <juliano.campos@amcom.com.br>
2024-05-08 14:17:27 -0400

    sugestoes por situação

--- SME.ConectaFormacao.TesteIntegracao/CasosDeUso/Proposta/Ao_obter_sugestao_parecer_pareceristas.cs
+++ SME.ConectaFormacao.TesteIntegracao/CasosDeUso/Proposta/Ao_obter_sugestao_parecer_pareceristas.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using Bogus;
+using Shouldly;
 using SME.ConectaFormacao.Aplicacao.Interfaces.Proposta;
 using SME.ConectaFormacao.Dominio.Entidades;
 using SME.ConectaFormacao.Dominio.Enumerados;
@@ -13,20 +14,22 @@ namespace SME.ConectaFormacao.TesteIntegracao.CasosDeUso.Proposta
         {
         }
 
-        [Fact(DisplayName = "Proposta - deve obter sugestões de parecer dos pareceristas aprovado")]
-        public async Task Deve_obter_Sugestoes_Parecer_Pareceristas_aprovado()
+        [Fact(DisplayName = "Proposta - deve obter sugestões de parecer dos pareceristas por situação")]
+        public async Task Deve_obter_Sugestoes_Parecer_Pareceristas_por_situacao()
         {
             // arrange 
             var proposta = await InserirNaBaseProposta(SituacaoProposta.AguardandoAnaliseParecerDF, quantidadeParecerista: 100);
 
+            var situacao = new Faker().PickRandom<SituacaoParecerista>();
+
             var casoDeUso = ObterCasoDeUso<ICasoDeUsoObterSugestaoParecerPareceristas>();
 
             // act 
-            var retorno = await casoDeUso.Executar(proposta.Id, SituacaoParecerista.Aprovada);
+            var retorno = await casoDeUso.Executar(proposta.Id, situacao);
 
             // assert
             var pareceristas = ObterTodos<PropostaParecerista>();
-            var aprovadas = pareceristas.Where(w => w.PropostaId == proposta.Id && w.Situacao == SituacaoParecerista.Aprovada).OrderBy(o => o.Id).Select(t => t.Justificativa);
+            var aprovadas = pareceristas.Where(w => w.PropostaId == proposta.Id && w.Situacao == situacao).OrderBy(o => o.Id).Select(t => t.Justificativa);
             var justificativas = string.Join('\n', aprovadas);
 
             retorno.ShouldBe(justificativas);
