786bdd2f2e57a27a87b2f10eb3c4441552fa272a
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-05-13 13:25:04 -0300

    Inclusão da situação para exibir propostas

--- SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioProposta.cs
+++ SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioProposta.cs
@@ -408,7 +408,7 @@ namespace SME.ConectaFormacao.Infra.Dados.Repositorios
                 query += " and p.numero_homologacao = @numeroHomologacao ";
 
             if (perfilUsuarioLogado.EhPerfilParecerista())
-                query += @" and p.situacao = @situacaoAguardandoParecerista 
+                query += @" and p.situacao = ANY(@situacaoAguardandoParecerista) 
                             and p.id in (select proposta_id from proposta_parecerista where not excluido and registro_funcional = @loginUsuarioLogado)";
 
             query += " ORDER BY coalesce(pm.criado_em, p.alterado_em, p.criado_em) DESC ";
@@ -427,7 +427,7 @@ namespace SME.ConectaFormacao.Infra.Dados.Repositorios
                 situacao,
                 formacaoHomologada,
                 situacoesProposta = situacoesProposta.Select(t => (int)t).ToArray(),
-                situacaoAguardandoParecerista = SituacaoProposta.AguardandoAnalisePeloParecerista,
+                situacaoAguardandoParecerista = new []{ (int)SituacaoProposta.AguardandoAnalisePeloParecerista,(int)SituacaoProposta.AguardandoReanalisePeloParecerista},
                 loginUsuarioLogado
             };
             return conexao.Obter().QueryAsync<Proposta>(query, parametros);
