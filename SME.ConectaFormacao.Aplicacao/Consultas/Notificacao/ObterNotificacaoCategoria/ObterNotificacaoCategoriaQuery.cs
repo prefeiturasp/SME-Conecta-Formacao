3eec5892d152aed5b306fbe2f6fc179dd22a3d08
Juliano Martins <juliano.campos@amcom.com.br>
2024-05-24 17:03:32 -0400

    Task 122364: Criar endpoint categoria, tipo e situação notificação

---
+++ SME.ConectaFormacao.Aplicacao/Consultas/Notificacao/ObterNotificacaoCategoria/ObterNotificacaoCategoriaQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SME.ConectaFormacao.Aplicacao.Dtos;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class ObterNotificacaoCategoriaQuery : IRequest<IEnumerable<RetornoListagemDTO>>
+    {
+        private static ObterNotificacaoCategoriaQuery _instancia;
+        public static ObterNotificacaoCategoriaQuery Instancia() => _instancia ??= new();
+    }
+}
--- SME.ConectaFormacao.Webapi/Controllers/NotificacaoController.cs
+++ SME.ConectaFormacao.Webapi/Controllers/NotificacaoController.cs
@@ -7,13 +7,43 @@ using SME.ConectaFormacao.Aplicacao.Interfaces.Notificacao;
 
 namespace SME.ConectaFormacao.Webapi.Controllers
 {
+    //[Authorize("Bearer")]
     public class NotificacaoController : BaseController
     {
+        [HttpGet("categoria")]
+        [ProducesResponseType(typeof(IEnumerable<RetornoListagemDTO>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
+        public async Task<IActionResult> ObterCategoriaNotificacao(
+            [FromServices] ICasoDeUsoObterCategoriaNotificacao casoDeUso)
+        {
+            return Ok(await casoDeUso.Executar());
+        }
+
+        [HttpGet("tipo")]
+        [ProducesResponseType(typeof(IEnumerable<RetornoListagemDTO>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
+        public async Task<IActionResult> ObterTipoNotificacao(
+            [FromServices] ICasoDeUsoObterTipoNotificacao casoDeUso)
+        {
+            return Ok(await casoDeUso.Executar());
+        }
+
+        [HttpGet("situacao")]
+        [ProducesResponseType(typeof(IEnumerable<RetornoListagemDTO>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
+        [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
+        public async Task<IActionResult> ObterSituacaoNotificacao(
+            [FromServices] ICasoDeUsoObterSituacaoNotificacao casoDeUso)
+        {
+            return Ok(await casoDeUso.Executar());
+        }
+
         [HttpGet("nao-lida")]
         [ProducesResponseType(typeof(long), 200)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
-        [Authorize("Bearer")]
         public async Task<IActionResult> ObterTotalNotificacaoNaoLida(
             [FromServices] ICasoDeUsoObterTotalNotificacaoNaoLida casoDeUso)
         {
@@ -24,7 +54,6 @@ namespace SME.ConectaFormacao.Webapi.Controllers
         [ProducesResponseType(typeof(NotificacaoDTO), 200)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
-        [Authorize("Bearer")]
         public async Task<IActionResult> ObterNotificacao(
             [FromServices] ICasoDeUsoObterTotalNotificacaoNaoLida casoDeUso)
         {
@@ -35,7 +64,6 @@ namespace SME.ConectaFormacao.Webapi.Controllers
         [ProducesResponseType(typeof(PaginacaoResultadoDTO<NotificacaoPaginadoDTO>), 200)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
         [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
-        [Authorize("Bearer")]
         public async Task<IActionResult> ObterNotificacoes(
             [FromServices] ICasoDeUsoObterNotificacaoPaginada casoDeUso,
             [FromQuery] NotificacaoFiltroDTO filtro)
