b425f84bbca3aae20373737ef327a34b77aec08a
Juliano Martins <juliano.campos@amcom.com.br>
2024-05-27 18:10:38 -0400

    enviar notificacao via signalR ao ser lida

---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Enviar/EnviarNotificacaoLida/EnviarNotificacaoLidaCommand.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.ConectaFormacao.Aplicacao.Dtos.Notificacao;
+
+namespace SME.ConectaFormacao.Aplicacao
+{
+    public class EnviarNotificacaoLidaCommand : IRequest<bool>
+    {
+        public EnviarNotificacaoLidaCommand(NotificacaoSignalRDTO notificacao)
+        {
+            Notificacao = notificacao;
+        }
+
+        public NotificacaoSignalRDTO Notificacao { get; }
+    }
+
+    public class EnviarNotificacaoLidaCommandValidator : AbstractValidator<EnviarNotificacaoLidaCommand>
+    {
+        public EnviarNotificacaoLidaCommandValidator()
+        {
+            RuleFor(c => c.Notificacao)
+                .NotNull()
+                .WithMessage("A notificação deve ser informada para o envio via signalR.");
+
+            RuleFor(c => c.Notificacao.Id)
+                .NotEmpty()
+                .WithMessage("O identificador da notificação deve ser informado para o envio via signalR.");
+            
+            RuleFor(c => c.Notificacao.Titulo)
+                .NotEmpty()
+                .WithMessage("O título da mensagem deve ser informada para o envio via signalR.");
+
+            RuleFor(c => c.Notificacao.Usuarios)
+                .NotNull()
+                .WithMessage("Os usuários devem ser informados para o envio via signalR.");
+        }
+    }
+}
