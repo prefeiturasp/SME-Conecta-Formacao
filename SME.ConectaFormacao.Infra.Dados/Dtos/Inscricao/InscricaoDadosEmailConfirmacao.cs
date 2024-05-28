203e8440adf0024bff11403375989ecfed4cac71
Thiago Fernando <thiago.fernando@msn.com>
2024-05-28 15:14:52 -0300

    Criado Estrutura do Email de confirmação

---
+++ SME.ConectaFormacao.Infra.Dados/Dtos/Inscricao/InscricaoDadosEmailConfirmacao.cs
@@ -0,0 +1,14 @@
+namespace SME.ConectaFormacao.Infra;
+
+public class InscricaoDadosEmailConfirmacao
+{
+    public long UsuarioId { get; set; }
+    public string Email { get; set; }
+    public DateTime DataInicio { get; set; }
+    public DateTime? DataFim { get; set; }
+    public string HoraInicio { get; set; }
+    public string HoraFim { get; set; }
+    public string Local { get; set; }
+    public bool IntegradoSga { get; set; }
+    public string nomeFormacao { get; set; }
+}
