612502e3318d8b65c36243ef0d76d9a3e7d72085
Thiago Fernando <thiago.fernando@msn.com>
2024-05-28 08:09:20 -0300

    Download do anexo

--- SME.ConectaFormacao.Aplicacao/Dtos/Inscricao/DadosListagemInscricaoDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Inscricao/DadosListagemInscricaoDTO.cs
@@ -15,5 +15,6 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Inscricao
         public string? Origem { get; set; }
         public bool IntegrarNoSga { get; set; }
         public bool Iniciado { get; set; }
+        public List<DadosAnexosInscricao> Anexos { get; set; } = new List<DadosAnexosInscricao>();
     }
 }
