29bb266b8136f56f96eb77919a4b357ad479c2ee
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-06-05 09:15:28 -0300

    Removendo a formatação de pontuação final

--- SME.ConectaFormacao.Aplicacao/Comandos/Email/CancelarIncricao/EnviarEmailCancelarInscricaoCommandHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Comandos/Email/CancelarIncricao/EnviarEmailCancelarInscricaoCommandHandler.cs
@@ -75,7 +75,7 @@ namespace SME.ConectaFormacao.Aplicacao
             mensagem.AppendLine($"<p>A sua inscrição na formação {nomeFormacao} foi cancelada.</p>");
 
             if (motivoCancelamento.EstaPreenchido())
-                mensagem.AppendLine(@$" <p>Motivo: {motivoCancelamento}.</p>");
+                mensagem.AppendLine(@$" <p>Motivo: {motivoCancelamento}</p>");
 
             mensagem.AppendLine(@"<div class=""footer"">
                                     <p>Acesse a nossa <a href=""https://conectaformacao.sme.prefeitura.sp.gov.br/area-publica"" class=""link"">área pública</a> e fique por dentro de todas as formações e eventos.</p>
