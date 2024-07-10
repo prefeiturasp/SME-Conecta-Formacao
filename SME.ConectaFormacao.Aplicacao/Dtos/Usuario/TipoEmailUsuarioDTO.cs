bdc0b3b1618c7dc821591b1ee7225b647592e60a
Jailson Volnei dos Santos <jailsvs@gmail.com>
2024-07-10 10:09:52 -0300

    123212 - TEstes integrados

--- SME.ConectaFormacao.Aplicacao/Dtos/Usuario/TipoEmailUsuarioDTO.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/Usuario/TipoEmailUsuarioDTO.cs
@@ -5,7 +5,7 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.Usuario
 {
     public class TipoEmailUsuarioDTO
     {
-        [Required(ErrorMessage = "É necessário informar o tipo de e-mail"), Range((int)TipoEmail.FuncionarioUnidadeParceira, (int)TipoEmail.Estagiario, ErrorMessage = "Tipo de e-mail inválido.")]
+        [Required(ErrorMessage = "É necessário informar o tipo de e-mail"), Range((int)TipoEmail.FuncionarioUnidadeParceira, (int)TipoEmail.Estagiario, ErrorMessage = "Tipo de e-mail inválido")]
         public int Tipo { get; set; }
     }
 }
