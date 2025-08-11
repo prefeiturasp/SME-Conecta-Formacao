a0091408d7ead32daf354099c0624df721453742
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-08-11 08:47:43 -0300

    Falta voltar e verificar

--- SME.ConectaFormacao.Dominio/Entidades/Inscricao.cs
+++ SME.ConectaFormacao.Dominio/Entidades/Inscricao.cs
@@ -1,4 +1,6 @@
 ﻿using SME.ConectaFormacao.Dominio.Enumerados;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SME.ConectaFormacao.Dominio.Entidades
 {
@@ -18,6 +20,8 @@ namespace SME.ConectaFormacao.Dominio.Entidades
         public long? ArquivoId { get; set; }
         public int? TipoVinculo { get; set; }
         public SituacaoInscricao Situacao { get; set; }
+
+        [Column("situacaoanterior")]
         public SituacaoInscricao? SituacaoAnterior { get; set; }
         public OrigemInscricao Origem { get; set; }
         public string MotivoCancelamento { get; set; }
