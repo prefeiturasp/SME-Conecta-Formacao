b15fe0f843c8355d2a53648e3ddcfb7d8ae581c1
Diego Moreno <dierudito@gmail.com>
2026-02-03 09:33:45 -0300

    Adicionar nova tentativa de envio de e-mail e aprimorar o repositório de parâmetros.
    AB#142192

--- SME.ConectaFormacao.Infra.Dados/Repositorios/Interfaces/IRepositorioParametroSistema.cs
+++ SME.ConectaFormacao.Infra.Dados/Repositorios/Interfaces/IRepositorioParametroSistema.cs
@@ -6,7 +6,7 @@ namespace SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces
 {
     public interface IRepositorioParametroSistema : IRepositorioBaseAuditavel<ParametroSistema>
     {
-        Task<ParametroSistema> ObterParametroPorTipoEAno(TipoParametroSistema tipoParametroSistema, int ano);
+        Task<ParametroSistema?> ObterParametroPorTipoEAno(TipoParametroSistema tipoParametroSistema, int ano = 0);
         Task<IEnumerable<string>> ObterDominiosPermitidosParaUesParceiras();
     }
 }
