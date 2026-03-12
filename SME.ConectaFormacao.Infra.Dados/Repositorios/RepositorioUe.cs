b18305c2922244905fe99531a6537e9a215f25bb
Diego Moreno <dierudito@gmail.com>
2026-03-12 17:36:37 -0300

    feat: Adiciona autocomplete de nome de UE com endpoint e testes
    Adiciona funcionalidade para autocompletar nome de Unidade Escolar (UE),
    incluindo use case, DTOs, repositório, endpoint na UeController e integração
    via IoC. Implementa testes unitários para o caso de uso e controller,
    permitindo busca paginada por nome de UE filtrada por DRE.

---
+++ SME.ConectaFormacao.Infra.Dados/Repositorios/RepositorioUe.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using SME.ConectaFormacao.Infra.Dados.Dtos;
+using SME.ConectaFormacao.Infra.Dados.Dtos.Ues;
+using SME.ConectaFormacao.Infra.Dados.Repositorios.Interfaces;
+
+namespace SME.ConectaFormacao.Infra.Dados.Repositorios
+{
+    public class RepositorioUe(IConectaFormacaoConexao conexao) : IRepositorioUe
+    {
+        public async Task<ResultadoPaginado<AutocompletarNomeUeDto>> AutocompletarNomeAsync(string termo, long dreId, int numeroPagina, int numeroRegistros)
+        {
+            termo = $"{termo}%";
+            const string sqlBase = """
+            FROM UE 
+            WHERE f_unaccent(SIGLA_TIPO_ESCOLA || ' ' || NOME_ESCOLA) ILIKE f_unaccent(@TermoBusca) AND UE.DRE_ID = @DreId
+            """;
+
+            const string sql = $"""
+            SELECT 
+                id AS Id, 
+                (SIGLA_TIPO_ESCOLA || ' ' || NOME_ESCOLA) AS Nome 
+            {sqlBase}
+            ORDER BY Nome
+            LIMIT @limit OFFSET @offset;
+            """;
+            const string sqlCount = $"SELECT COUNT(1) {sqlBase}";
+            var conn = conexao.Obter();
+            var totalRegistros = await conn.ExecuteScalarAsync<int>(sqlCount, new { termo, dreId });
+            if (totalRegistros == 0)
+                return new()
+                {
+                    Itens = [],
+                    TotalRegistros = 0,
+                    PaginaAtual = numeroPagina,
+                    TamanhoPagina = numeroRegistros
+                };
+
+            var registrosIgnorados = (numeroPagina - 1) * numeroRegistros;
+
+            var itens = await conn.QueryAsync<AutocompletarNomeUeDto>(sql, new { TermoBusca = termo, DreId = dreId, limit = numeroRegistros, offset = registrosIgnorados });
+
+            return new()
+            {
+                Itens = itens,
+                TotalRegistros = totalRegistros,
+                PaginaAtual = numeroPagina,
+                TamanhoPagina = numeroRegistros
+            };
+        }
+    }
+}
---
+++ SME.ConectaFormacao.Webapi/Controllers/UeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.ConectaFormacao.Aplicacao.Interfaces.Ues;
+using SME.ConectaFormacao.Infra.Dados.Dtos.Ues;
+
+namespace SME.ConectaFormacao.Webapi.Controllers
+{
+    [Authorize("Bearer")]
+    public class UeController (ICasoDeUsoObterAutocompletarNomeUe casoDeUsoAutocompletarNome) : BaseController
+    {
+        [HttpGet("autocompletar-nome")]
+        public async Task<IActionResult> AutocompletarNomeAsync([FromQuery] FiltroAutocompletarNomeUeDto filtro)
+        {
+            return ProcessarResultado(await casoDeUsoAutocompletarNome.ExecutarAsync(filtro));
+        }
+    }
+}
