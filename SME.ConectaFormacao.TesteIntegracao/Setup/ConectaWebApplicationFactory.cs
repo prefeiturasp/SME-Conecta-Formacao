73691fb6de50e6a4ae2f71a87c8959ad38e6dafb
Diego Moreno <dierudito@gmail.com>
2026-03-30 08:05:17 -0300

    test: Adiciona ConectaWebApplicationFactory para testes
    Adiciona a classe ConectaWebApplicationFactory herdando de WebApplicationFactory<Program> e implementando IAsyncLifetime do xUnit, com métodos InitializeAsync e DisposeAsync ainda não implementados. Prepara a base para testes de integração.

---
+++ SME.ConectaFormacao.TesteIntegracao/Setup/ConectaWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+namespace SME.ConectaFormacao.TesteIntegracao.Setup
+{
+    public class ConectaWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
