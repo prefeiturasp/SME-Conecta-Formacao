99b3f462310f28f07bf311629b76eb7d0ceb0a89
Diego Moreno <dierudito@gmail.com>
2026-04-01 14:54:53 -0300

    feat: Implementa delta de inscritos na lista de presença CODAF
    Adiciona enum TipoDeltaInscritoCodaf, DTO de resultado e query para identificar alterações (novo, removido, sem alteração, ignorado) nos inscritos de uma turma CODAF. Atualiza repositório e interface para expor método de obtenção do delta e inclui campo Login no DTO de inscrito. Facilita sincronização e relatórios de modificações na lista de presença.
    
    AB#141574

---
+++ SME.ConectaFormacao.Dominio/Enumerados/TipoDeltaInscritoCodaf.cs
@@ -0,0 +1,10 @@
+﻿namespace SME.ConectaFormacao.Dominio.Enumerados
+{
+    public enum TipoDeltaInscritoCodaf
+    {
+        SemAlteracao = 0,
+        Novo = 1,
+        Removido = 2,
+        Ignorado = -1
+    }
+}
---
+++ SME.ConectaFormacao.Infra.Dados/Dtos/CodafListaPresencas/ResultadoDeltaInscritoCodafDto.cs
@@ -0,0 +1,10 @@
+﻿using SME.ConectaFormacao.Dominio.Enumerados;
+
+namespace SME.ConectaFormacao.Infra.Dados.Dtos.CodafListaPresencas
+{
+    public class ResultadoDeltaInscritoCodafDto
+    {
+        public TipoDeltaInscritoCodaf TipoDelta { get; set; }
+        public ResultadoInscritoTurmaCodafListaPresencaDto DadosInscrito { get; set; } = new();
+    }
+}
