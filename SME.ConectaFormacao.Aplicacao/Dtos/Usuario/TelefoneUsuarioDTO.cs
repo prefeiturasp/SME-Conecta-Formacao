bb8cfe3c73ceb3529b208f09bd3bf775e50b1cd7
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-06-03 14:17:25 -0300

    Adicionar campo de telefone na tela de meus dados + testes unitários

---
+++ SME.ConectaFormacao.Aplicacao/Dtos/Usuario/TelefoneUsuarioDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace SME.ConectaFormacao.Aplicacao.Dtos.Usuario
+{
+    public class TelefoneUsuarioDTO
+    {
+        public string Telefone { get; set; } = string.Empty;
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Interfaces/Usuario/ICasoDeUsoUsuarioAlterarTelefone.cs
@@ -0,0 +1,7 @@
+﻿namespace SME.ConectaFormacao.Aplicacao.Interfaces.Usuario
+{
+    public interface ICasoDeUsoUsuarioAlterarTelefone
+    {
+        Task<bool> Executar(string login, string nome);
+    }
+}
