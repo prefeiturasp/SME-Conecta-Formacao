5e089ccb908bf0865ccfbd58b9e8124d8d62a3ee
Diego Moreno <dierudito@gmail.com>
2026-06-19 09:20:33 -0300

    feat: Adiciona validação para cadastro de codaf suplementar

--- SME.ConectaFormacao.Aplicacao/CasosDeUso/CodafSuplementares/CasoDeUsoCriarCodafSuplementar.cs
+++ SME.ConectaFormacao.Aplicacao/CasosDeUso/CodafSuplementares/CasoDeUsoCriarCodafSuplementar.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using FluentValidation;
+﻿using FluentValidation;
 using SME.ConectaFormacao.Aplicacao.Dtos.CodafSuplementares;
 using SME.ConectaFormacao.Aplicacao.Interfaces.CodafSuplementares;
 using SME.ConectaFormacao.Dominio.Comum;
@@ -11,8 +10,7 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.CodafSuplementares
     public class CasoDeUsoCriarCodafSuplementar(
         IRepositorioCodafSuplementar repositorioCodafSuplementar,
         IRepositorioCodafListaPresenca repositorioCodafLista,
-        IValidator<CodafSuplementarCadastroDto> validator,
-        IMapper mapper) : ICasoDeUsoCriarCodafSuplementar
+        IValidator<CodafSuplementarCadastroDto> validator) : ICasoDeUsoCriarCodafSuplementar
     {
         public async Task<Resultado<CodafSuplementarDetalhadoDto>> ExecutarAsync(CodafSuplementarCadastroDto codafSuplementarCadastroDto)
         {
@@ -24,6 +22,11 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.CodafSuplementares
 
             if (codafOriginal is null)
                 return Erro.Validacao("Codaf não encontrado para a turma informada");
+
+            var codafSuplementarExistente = await repositorioCodafSuplementar.ObterPorExpressaoAsync(c => c.CodafListaPresencaId == codafOriginal.Id);
+
+            if (codafSuplementarExistente is not null)
+                return Erro.Validacao("Já existe um codaf suplementar para a turma informada");
 
             var codafSuplementar = new CodafSuplementar(codafSuplementarCadastroDto.CodafId,
                 new(codafSuplementarCadastroDto.DataPublicacao,
@@ -37,7 +40,24 @@ namespace SME.ConectaFormacao.Aplicacao.CasosDeUso.CodafSuplementares
             var id = await repositorioCodafSuplementar.Inserir(codafSuplementar);
             codafSuplementar.Id = id;
 
-            return mapper.Map<CodafSuplementarDetalhadoDto>(codafSuplementar);
+            var codafSuplementarDetalhadoDto = new CodafSuplementarDetalhadoDto
+            {
+                AlteradoEm = codafSuplementar.AlteradoEm,
+                AlteradoLogin = codafSuplementar.AlteradoLogin,
+                AlteradoPor = codafSuplementar.AlteradoPor,
+                CodigoCursoEol = codafSuplementar.CodigoCursoEol,
+                CodigoNivel = codafSuplementar.CodigoNivel,
+                CriadoEm = codafSuplementar.CriadoEm,
+                CriadoLogin = codafSuplementar.CriadoLogin,
+                CriadoPor = codafSuplementar.CriadoPor,
+                DataPublicacao = codafSuplementar.DataPublicacao,
+                DataPublicacaoDom = codafSuplementar.DataPublicacaoDom,
+                Id = codafSuplementar.Id,
+                CodigoFormacao = codafOriginal.PropostaId,
+                PropostaId = codafOriginal.PropostaId
+            };
+
+            return codafSuplementarDetalhadoDto;
         }
     }
 }
--- SME.ConectaFormacao.Aplicacao/Dtos/CodafSuplementares/CodafSuplementarCadastroDto.cs
+++ SME.ConectaFormacao.Aplicacao/Dtos/CodafSuplementares/CodafSuplementarCadastroDto.cs
@@ -1,4 +1,5 @@
-﻿using SME.ConectaFormacao.Aplicacao.Dtos.Codaf;
+﻿using FluentValidation;
+using SME.ConectaFormacao.Aplicacao.Dtos.Codaf;
 
 namespace SME.ConectaFormacao.Aplicacao.Dtos.CodafSuplementares
 {
@@ -15,5 +16,23 @@ namespace SME.ConectaFormacao.Aplicacao.Dtos.CodafSuplementares
         public IList<CodafSuplementarInscritoSalvarDto>? Inscritos { get; set; }
         public IList<CodafSuplementarRetificacaoSalvarDto>? Retificacoes { get; set; }
         public IList<CodafAnexoSalvarDto>? Anexos { get; set; }
+    }
+
+    public class CodafSuplementarCadastroValidator : AbstractValidator<CodafSuplementarCadastroDto>
+    {
+        public CodafSuplementarCadastroValidator()
+        {
+            RuleFor(c => c.CodafId).GreaterThan(0).WithMessage("O Id do Codaf é obrigatório.");
+            RuleForEach(c => c.Inscritos).SetValidator(new CodafSuplementarInscritoSalvarValidator());
+            RuleFor(c => c.Inscritos)
+                .Must(inscritos =>
+                {
+                    if (inscritos is null)
+                        return true;
+                    var idsInscritos = inscritos.Select(i => i.InscricaoId).ToList();
+                    return idsInscritos.Distinct().Count() == idsInscritos.Count;
+                })
+                .WithMessage("Existem inscritos duplicados no codaf suplementar.");
+        }
     }
 }
