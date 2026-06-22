8bd55becf9942c2589aa8299194d5be82fb42b26
Diego Moreno <dierudito@gmail.com>
2026-06-22 09:59:23 -0300

    feat: Adiciona mapeamentos para retificações e anexos no Codaf Suplementar

--- SME.ConectaFormacao.Aplicacao/Mapeamentos/CodafSuplementarProfile.cs
+++ SME.ConectaFormacao.Aplicacao/Mapeamentos/CodafSuplementarProfile.cs
@@ -12,17 +12,22 @@ namespace SME.ConectaFormacao.Aplicacao.Mapeamentos
         public CodafSuplementarProfile()
         {
             CreateMap<CodafListaPresenca, CodafSuplementarDetalhadoDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
                 .ForMember(dest => dest.NomeFormacao, opt => opt.MapFrom(src => src.Proposta.NomeFormacao))
                 .ForMember(dest => dest.CodigoFormacao, opt => opt.MapFrom(src => src.Proposta.Id))
                 .ForMember(dest => dest.NumeroHomologacao, opt => opt.MapFrom(src => src.Proposta.NumeroHomologacao))
                 .ForMember(dest => dest.Retificacoes, opt => opt.MapFrom(src => src.CodafRetificacoes))
                 .ForMember(dest => dest.Anexos, opt => opt.MapFrom(src => src.CodafAnexos))
+                .ReverseMap()
                 ;
             CreateMap<CodafSuplementarRetificacao, CodafSuplementarRetificacaoDto>();
             CreateMap<CodafAnexoSalvarDto, CodafSuplementarAnexo>()
                 .ForMember(dest => dest.Extensao, opt => opt.MapFrom(src => src.NomeArquivo.Substring(src.NomeArquivo.LastIndexOf('.') + 1)))
                 ;
             CreateMap<CodafSuplementarAnexo, CodafSuplementarAnexoDto>().ForMember(dest => dest.UrlDownload, opt => opt.Ignore());
+            CreateMap<CodafRetificacaoListaPresenca, CodafSuplementarRetificacaoDto>();
+            CreateMap<CodafAnexo, CodafSuplementarAnexoDto>().ForMember(dest => dest.UrlDownload, opt => opt.Ignore());
+
         }
     }
 }
