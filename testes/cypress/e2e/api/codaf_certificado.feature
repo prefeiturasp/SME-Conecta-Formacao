9bcb1730de658559fe8f76d3154b03fbc5db34f2
Alex Paulo <alexpaulo.pc@gmail.com>
2026-06-22 08:39:56 -0300

    Criação dos testes da api CodafCertificado.

---
+++ testes/cypress/e2e/api/codaf_certificado.feature
@@ -0,0 +1,73 @@
+# language: pt
+
+Funcionalidade: API - Codaf Certificado
+
+  Cenário: Retornar todos meus certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint meus CodafCertificado
+    Então retorna o status 200 com meus certificados
+
+  Cenário: Retornar meus certificados como cursista
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint meus CodafCertificado cursista
+    Então retorna o status 200 com meus certificados de cursista
+
+  Cenário: Não retornar meus certificados sem autenticação
+    Dado que não possuo um token válido
+    Quando tento a requisição GET no endpoint meus CodafCertificado
+    Então retorna o status 401 sem meus certificados
+
+  Cenário: Retornar Codaf certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint CodafCertificado
+    Então retorna o status 200 com Codaf certificados
+
+  Cenário: Retornar Codaf certificados de cursista
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint CodafCertificado cursista
+    Então retorna o status 200 com Codaf certificados de cursista
+
+  Cenário: Não retornar Codaf certificados sem autenticação
+    Dado que não possuo um token válido
+    Quando tento a requisição GET no endpoint CodafCertificado
+    Então retorna o status 401 sem Codaf certificados
+
+  Cenário: Retornar download Codaf certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint CodafCertificado download
+    Então retorna o status 200 com download Codaf certificados
+
+  Cenário: Id Codaf obrigatório para download certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição GET no endpoint sem id CodafCertificado download
+    Então retorna o status 404 sem download Codaf certificados
+
+  Cenário: Não retornar download Codaf certificados sem autenticação
+    Dado que não possuo um token válido
+    Quando tento a requisição GET no endpoint CodafCertificado download
+    Então retorna o status 401 sem download Codaf certificados
+
+  Cenário: Emitir download por lista presença Codaf certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição POST no endpoint CodafCertificado download lista
+    Então retorna o status 200 com emitindo download lista presença Codaf certificados
+
+  Cenário: Não emitir download sem a lista presença Codaf certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição POST no endpoint CodafCertificado download sem lista
+    Então retorna o status 404 sem download lista presença Codaf certificados
+
+  Cenário: Não emitir download por lista presença Codaf certificados sem autenticação
+    Dado que não possuo um token válido
+    Quando tento a requisição POST no endpoint CodafCertificado download lista
+    Então retorna o status 401 sem emitir download lista presença Codaf certificados
+
+  Cenário: Emitir download lote Codaf certificados
+    Dado que possuo um token válido do CodafCertificado
+    Quando envio uma requisição POST no endpoint CodafCertificado download lote
+    Então retorna o status 200 com emitindo download lote Codaf certificados
+
+  Cenário: Não emitir download lote Codaf certificados sem autenticação
+    Dado que não possuo um token válido
+    Quando tento a requisição POST no endpoint CodafCertificado download lote
+    Então retorna o status 401 sem emitir download lote Codaf certificados
---
+++ testes/cypress/support/step_definitions/codaf_certificado.js
@@ -0,0 +1,344 @@
+import { Given, When, Then, Before } from "@badeball/cypress-cucumber-preprocessor"
+
+const Dado = Given
+const Quando = When
+const Então = Then
+
+let token
+
+Before(() => {
+  cy.gerar_token().then((token_valido) => {
+    token = token_valido
+  })
+})
+
+Dado('que possuo um token válido do CodafCertificado', function () {
+  expect(token, 'valido').to.exist
+})
+
+// Retornar todos meus certificados
+Quando('envio uma requisição GET no endpoint meus CodafCertificado', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/meus`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },         
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com meus certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(200)
+
+    expect(response.body).to.have.property('items').that.is.an('array')
+    expect(response.body).to.have.property('totalPaginas').that.is.a('number')
+    expect(response.body).to.have.property('totalRegistros').that.is.a('number')
+
+    response.body.items.forEach((certificado) => {
+      expect(certificado).to.have.property('id').that.is.a('number')
+      expect(certificado).to.have.property('numeroHomologacao').that.is.a('number')
+      expect(certificado).to.have.property('nomeFormacao').that.is.a('string')
+      expect(certificado).to.have.property('codigoCertificado').that.is.a('number')
+      expect(certificado).to.have.property('temRf').that.is.a('boolean')
+      expect(certificado).to.have.property('tipoParticipacao').that.is.a('number')
+      expect(certificado).to.have.property('dataEmissao').that.is.a('string')
+    })
+  })
+})
+
+// Retornar meus certificados como cursista
+Quando('envio uma requisição GET no endpoint meus CodafCertificado cursista', function () {
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/meus?TipoParticipacao=1`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com meus certificados de cursista', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(200)
+  })
+})
+
+// Não retornar meus certificados sem autenticação
+Quando('tento a requisição GET no endpoint meus CodafCertificado', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/meus`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `token_invalido`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 401 sem meus certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(401)
+  })
+})
+
+// Retornar Codaf certificados
+Quando('envio uma requisição GET no endpoint CodafCertificado', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },         
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(200)
+
+    expect(response.body).to.have.property('items').that.is.an('array')
+    expect(response.body).to.have.property('totalPaginas').that.is.a('number')
+    expect(response.body).to.have.property('totalRegistros').that.is.a('number')
+
+    response.body.items.forEach((certificado) => {
+      expect(certificado).to.have.property('id').that.is.a('number')
+      expect(certificado).to.have.property('numeroHomologacao').that.is.a('number')
+      expect(certificado).to.have.property('nomeFormacao').that.is.a('string')
+      expect(certificado).to.have.property('documento').that.is.a('string')
+      expect(certificado).to.have.property('codigoCertificado').that.is.a('number')
+      expect(certificado).to.have.property('tipoCertificado').that.is.a('number')
+      expect(certificado).to.have.property('dataEmissao').that.is.a('string')
+      expect(certificado).to.have.property('nomeParticipante').that.is.a('string')
+    })
+  })
+})
+
+// Retornar Codaf certificados de cursista
+Quando('envio uma requisição GET no endpoint CodafCertificado cursista', function () {
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado?TipoParticipacao=1`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com Codaf certificados de cursista', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(200)
+    
+    expect(response.body).to.have.property('items').that.is.an('array')
+    expect(response.body).to.have.property('totalPaginas').that.is.a('number')
+    expect(response.body).to.have.property('totalRegistros').that.is.a('number')
+
+    response.body.items.forEach((certificado) => {
+      expect(certificado).to.have.property('id').that.is.a('number')
+      expect(certificado).to.have.property('numeroHomologacao').that.is.a('number')
+      expect(certificado).to.have.property('nomeFormacao').that.is.a('string')
+      expect(certificado).to.have.property('documento').that.is.a('string')
+      expect(certificado).to.have.property('codigoCertificado').that.is.a('number')
+      expect(certificado).to.have.property('tipoCertificado').that.is.a('number')
+      expect(certificado).to.have.property('dataEmissao').that.is.a('string')
+      expect(certificado).to.have.property('nomeParticipante').that.is.a('string')
+    })
+  })
+})
+
+// Não retornar Codaf certificados sem autenticação
+Quando('tento a requisição GET no endpoint CodafCertificado', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/meus`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `token_invalido`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 401 sem Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(401)
+  })
+})
+
+// Retornar download Codaf certificados
+Quando('envio uma requisição GET no endpoint CodafCertificado download', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/572/download`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },         
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com download Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(200)
+
+    expect(response.body).to.have.property('id').that.is.a('number')
+    expect(response.body).to.have.property('codigoCertificado').that.is.a('number')
+    expect(response.body).to.have.property('urlDownload').that.is.a('string')
+    expect(response.body).to.have.property('nomeCompleto').that.is.a('string')
+    expect(response.body).to.have.property('nomeFormacao').that.is.a('string')
+  })
+})
+
+// Id Codaf obrigatório para download certificados
+Quando('envio uma requisição GET no endpoint sem id CodafCertificado download', function () {
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado//download`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 404 sem download Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(404)
+  })
+})
+
+// Não retornar download Codaf certificados sem autenticação
+Quando('tento a requisição GET no endpoint CodafCertificado download', function () { 
+  return cy.request({
+    method: 'GET',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/{certificadoCodafId}/download`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `token_invalido`
+    },          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 401 sem download Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(401)
+  })
+})
+
+// Emitir download por lista presença Codaf certificados
+Quando('envio uma requisição POST no endpoint CodafCertificado download lista', function () {
+  return cy.request({
+    method: 'POST',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/1/emitir`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },
+    body: 
+      [ 0 ] ,          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com emitindo download lista presença Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(204)
+  })
+})
+
+// Não emitir download sem a lista presença Codaf certificados
+Quando('envio uma requisição POST no endpoint CodafCertificado download sem lista', function () {
+  return cy.request({
+    method: 'POST',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado//emitir`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },
+    body: 
+      [ 0 ] ,          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 404 sem download lista presença Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(404)
+  })
+})
+
+
+// Não emitir download por lista presença Codaf certificados sem autenticação
+Quando('tento a requisição POST no endpoint CodafCertificado download lista', function () { 
+  return cy.request({
+    method: 'POST',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/1/emitir`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `token_invalido`
+    },
+    body: 
+      [ 0 ] ,           
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 401 sem emitir download lista presença Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(401)
+  })
+})
+
+// Emitir download lote Codaf certificados
+Quando('envio uma requisição POST no endpoint CodafCertificado download lote', function () {
+  return cy.request({
+    method: 'POST',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/download-lote`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `Bearer ${token}`
+    },
+    body: 
+      [ 0 ] ,          
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 200 com emitindo download lote Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(500)
+  })
+})
+
+// Não emitir download lote Codaf certificados sem autenticação
+Quando('tento a requisição POST no endpoint CodafCertificado download lote', function () { 
+  return cy.request({
+    method: 'POST',
+    url: Cypress.config('baseUrl') + `/api/v1/CodafCertificado/download-lote`,
+    headers: {
+      accept: 'text/plain',
+      Authorization: `token_invalido`
+    },
+    body: 
+      [ 0 ] ,           
+    failOnStatusCode: false  
+  }).as('response')
+})
+
+Então('retorna o status 401 sem emitir download lote Codaf certificados', function () {
+  cy.get('@response').then((response) => {
+    expect(response.status).to.eq(401)
+  })
+})
