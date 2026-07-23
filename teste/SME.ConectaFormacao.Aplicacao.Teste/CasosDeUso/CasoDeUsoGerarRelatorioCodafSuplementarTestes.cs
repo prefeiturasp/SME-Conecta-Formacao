a59e0e020d6faee10a17d6e7311e9f7df57381bf
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-07-23 15:29:51 -0300

    Ajuste em testes

--- teste/SME.ConectaFormacao.Aplicacao.Teste/CasosDeUso/CasoDeUsoGerarRelatorioCodafSuplementarTestes.cs
+++ teste/SME.ConectaFormacao.Aplicacao.Teste/CasosDeUso/CasoDeUsoGerarRelatorioCodafSuplementarTestes.cs
@@ -100,11 +100,7 @@ namespace SME.ConectaFormacao.Aplicacao.Teste.CasosDeUso
         {
             // Arrange
             var lista = CriarLista(StatusCodafListaPresenca.Finalizado);
-
-            typeof(CodafSuplementar)
-                .GetProperty(nameof(CodafSuplementar.Status))
-                ?.SetValue(codafSuplementar, StatusCodafSuplementar.Finalizado);
-
+            var codafSuplementar = CriarCodafSuplementar(StatusCodafSuplementar.Finalizado);
 
             repositorioMock
                 .Setup(r => r.ObterPorIdComPropostaEPropostaTurmaAsync(It.IsAny<long>()))
@@ -123,10 +119,6 @@ namespace SME.ConectaFormacao.Aplicacao.Teste.CasosDeUso
 
             // Assert
             Assert.True(resultado.Sucesso);
-
-            repositorioCodafMock.Verify(r =>
-                r.Atualizar(It.IsAny<CodafSuplementar>()),
-                Times.Never);
 
             repositorioCodafMock.Verify(r =>
                 r.Atualizar(It.IsAny<CodafSuplementar>()),
