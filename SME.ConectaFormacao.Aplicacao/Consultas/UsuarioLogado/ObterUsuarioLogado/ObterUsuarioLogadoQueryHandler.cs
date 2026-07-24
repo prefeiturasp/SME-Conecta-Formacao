304b5d667413e2bd6f019d4f0006cddda263ea94
Diego Moreno <dierudito@gmail.com>
2026-07-24 14:07:29 -0300

    feat: Adiciona mediador e log na execução do caso de uso para listar presenças

--- SME.ConectaFormacao.Aplicacao/Consultas/UsuarioLogado/ObterUsuarioLogado/ObterUsuarioLogadoQueryHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Consultas/UsuarioLogado/ObterUsuarioLogado/ObterUsuarioLogadoQueryHandler.cs
@@ -1,20 +1,17 @@
 using MediatR;
-using SME.ConectaFormacao.Aplicacao.Comandos.SalvarLog;
 using SME.ConectaFormacao.Dominio.Constantes;
 using SME.ConectaFormacao.Dominio.Contexto;
 using SME.ConectaFormacao.Dominio.Entidades;
 using SME.ConectaFormacao.Dominio.Extensoes;
-using SME.ConectaFormacao.Infra.Dominio.Enumerados;
 using SME.ConectaFormacao.Infra.Servicos.Cache;
 
 namespace SME.ConectaFormacao.Aplicacao
 {
-    public class ObterUsuarioLogadoQueryHandler(IContextoAplicacao contextoAplicacao, IMediator mediator, ICacheDistribuido cacheDistribuido) : 
+    public class ObterUsuarioLogadoQueryHandler(IContextoAplicacao contextoAplicacao, IMediator mediator, ICacheDistribuido cacheDistribuido) :
         IRequestHandler<ObterUsuarioLogadoQuery, Usuario>
     {
         public async Task<Usuario> Handle(ObterUsuarioLogadoQuery request, CancellationToken cancellationToken)
         {
-            await mediator.Send(new SalvarLogCommand(typeof(ObterUsuarioLogadoQueryHandler).FullName!, LogNivel.Informacao, "Iniciando consulta do usuário logado", null, Guid.NewGuid(), null), cancellationToken);
             var nomeChave = CacheDistribuidoNomes.UsuarioLogado.Parametros(contextoAplicacao.UsuarioLogado);
 
             return await cacheDistribuido.ObterAsync(nomeChave, () => mediator.Send(new ObterUsuarioPorLoginQuery(contextoAplicacao.UsuarioLogado), cancellationToken));
