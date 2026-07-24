3932f602404f922397d19bc7767c738511ce29cb
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-07-24 15:06:37 -0300

    Ajuste sonar

--- SME.ConectaFormacao.Aplicacao/Consultas/Inscricoes/ObterDadosPaginadosComFiltrosQueryHandler.cs
+++ SME.ConectaFormacao.Aplicacao/Consultas/Inscricoes/ObterDadosPaginadosComFiltrosQueryHandler.cs
@@ -46,7 +46,7 @@ namespace SME.ConectaFormacao.Aplicacao
                 var inscricao = turmasFormacao?.Where(x => x.PropostaId == proposta.Id) ?? Enumerable.Empty<ListagemFormacaoComTurmaDTO>();
 
                 var turmas = inscricao
-                    .Where(i => !(apenasSemCodaf == true) || !i.CodafId.HasValue)
+                    .Where(i => (apenasSemCodaf != true) || !i.CodafId.HasValue)
                     .Select(i => new DadosListagemFormacaoTurma
                     {
                         PropostaTurmaId = i.PropostaTurmaId,
