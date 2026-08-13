1637fdbe9acb66af9a5456d891f822fbaa8775bd
Diego Moreno <dierudito@gmail.com>
2026-08-13 17:49:11 -0300

    fix: Atualiza mensagem de certificação para maior clareza

--- SME.ConectaFormacao.Infra.Dados/Estrategias/CodafCertificados/CertificadoCursistaComRfStrategy.cs
+++ SME.ConectaFormacao.Infra.Dados/Estrategias/CodafCertificados/CertificadoCursistaComRfStrategy.cs
@@ -35,7 +35,7 @@ namespace SME.ConectaFormacao.Infra.Dados.Estrategias.CodafCertificados
 
         private static string GerarCorpoCertificado(DadosEmissaoCertificadoCodafDto dados)
         {
-            return $@"Declaramos para os devidos fins que o(a) servidor(a), <b><i>{StringExtensao.FormatarNomePessoa(dados.NomeExibicao)}</i></b>,
+            return $@"Certificamos para os devidos fins que o(a) servidor(a), <b><i>{StringExtensao.FormatarNomePessoa(dados.NomeExibicao)}</i></b>,
                     RF: <b><i>{StringExtensao.AplicarMascaraRf(dados.Documento)}</i></b>, participou do {dados.TipoFormacao} <b><i>{dados.NomeFormacao}</i></b> 
                     promovido pela {ObterTextoEmissorCorpo(dados)} da Secretaria Municipal de Educação, no período de {dados.DataInicio:dd/MM/yyyy} a {dados.DataFim:dd/MM/yyyy}, 
                     com carga horária de {dados.DefinirCargaHoraria()} horas,
