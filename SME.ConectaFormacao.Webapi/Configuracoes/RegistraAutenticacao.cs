69c06ebdb10d9b39c17d89d9379c57ac6f999a77
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-08-13 15:01:58 -0300

    Adicionar flag de "possuiAprovacoes" na listagem de CODAF

--- SME.ConectaFormacao.Webapi/Configuracoes/RegistraAutenticacao.cs
+++ SME.ConectaFormacao.Webapi/Configuracoes/RegistraAutenticacao.cs
@@ -24,7 +24,8 @@ public static class RegistraAutenticacao
                 ValidIssuer = configuration.GetValue<string>("JwtTokenSettings:Issuer"),
                 ValidateIssuerSigningKey = true,
                 ClockSkew = TimeSpan.Zero,
-                IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes(configuration.GetValue<string>("JwtTokenSettings:IssuerSigningKey")))
+                //IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes(configuration.GetValue<string>("JwtTokenSettings:IssuerSigningKey")))
+                IssuerSigningKey = new SymmetricSecurityKey(UTF8.GetBytes("132CD0A7B1215897C5EF1CC6D7C7469C687D17FAZ85E675B6EBD9FBA26615B93805556652B2DDFD96CA2565C8D42EE83EF44CAC3B79AF64B343461B52ACC75FA"))
             };
         });
 
