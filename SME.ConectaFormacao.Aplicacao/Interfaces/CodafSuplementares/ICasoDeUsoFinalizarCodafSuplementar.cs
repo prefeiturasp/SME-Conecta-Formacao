aa96c101081bc8163df41d225b40a43545e8abc4
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-08-18 16:09:06 -0300

    Criar endpoint para finalizar CODAF Suplementar + teste

---
+++ SME.ConectaFormacao.Aplicacao/Interfaces/CodafSuplementares/ICasoDeUsoFinalizarCodafSuplementar.cs
@@ -0,0 +1,9 @@
+﻿using SME.ConectaFormacao.Dominio.Comum;
+
+namespace SME.ConectaFormacao.Aplicacao.Interfaces.CodafSuplementares
+{
+    public interface ICasoDeUsoFinalizarCodafSuplementar
+    {
+        Task<Resultado> ExecutarAsync(long codafSuplementarId);
+    }
+}
