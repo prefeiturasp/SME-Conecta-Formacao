81ab1de540cedb274cde9d2c93078316059025e4
Diego Moreno <dierudito@gmail.com>
2026-08-31 11:39:09 -0300

    Update IContextoAplicacao.cs

--- SME.ConectaFormacao.Dominio/Contexto/IContextoAplicacao.cs
+++ SME.ConectaFormacao.Dominio/Contexto/IContextoAplicacao.cs
@@ -4,7 +4,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SME.ConectaFormacao.Dominio.Contexto;
 
-[ExcludeFromCodeCoverage]
 public interface IContextoAplicacao
 {
     IDictionary<string, object> Variaveis { get; set; }
