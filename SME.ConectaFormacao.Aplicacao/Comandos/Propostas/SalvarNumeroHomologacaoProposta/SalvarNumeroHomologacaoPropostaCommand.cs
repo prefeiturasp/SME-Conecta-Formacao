25b748ee9bd4ddffc8bd87ef3736e4372fa6a36d
Diego Moreno <dierudito@gmail.com>
2026-09-09 09:00:20 -0300

    feat: Adiciona lógica para salvar número de homologação da proposta
    AB#155801
    AB#155802

---
+++ SME.ConectaFormacao.Aplicacao/Comandos/Propostas/SalvarNumeroHomologacaoProposta/SalvarNumeroHomologacaoPropostaCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SME.ConectaFormacao.Aplicacao.Comandos.Propostas.SalvarNumeroHomologacaoProposta
+{
+    public class SalvarNumeroHomologacaoPropostaCommand(long propostaId, long? numeroHomologacao) : IRequest<bool>
+    {
+        public long PropostaId { get; set; } = propostaId;
+        public long? NumeroHomologacao { get; set; } = numeroHomologacao;
+    }
+}
---
+++ SME.ConectaFormacao.Aplicacao/Interfaces/Proposta/ICasoDeUsoSalvarNumeroHomologacaoProposta.cs
@@ -0,0 +1,9 @@
+﻿using SME.ConectaFormacao.Aplicacao.Dtos.Proposta;
+
+namespace SME.ConectaFormacao.Aplicacao.Interfaces.Proposta
+{
+    public interface ICasoDeUsoSalvarNumeroHomologacaoProposta
+    {
+        Task<bool> Executar(long propostaId, PropostaNumeroHomologacaoDto dto);
+    }
+}
